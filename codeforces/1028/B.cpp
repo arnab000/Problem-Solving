7a215896aabb2d526ba6ef5211e80d68d5daf872
Arnab Saha <arnab.saintjoseph@gmail.com>
2018-09-04 23:49:00 +0600

    Add solution for problem `B - Unnatural Conditions` Link: https://codeforces.com/contest/1028/problem/B Tags: constructive algorithms, math, *1200 Ref: https://codeforces.com/contest/1028/submission/42469755

---
+++ codeforces/1028/B.cpp
@@ -0,0 +1,22 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   long long i,j,x,y,k;
+   cin>>i>>j;
+x=ceil(i*1.0/4);
+for(k=0;k<x-1;k++)
+{
+    cout<<"4";
+}
+cout<<"5";
+cout<<endl;
+for(k=0;k<x;k++)
+{
+    cout<<"5";
+}
+
+
+}
