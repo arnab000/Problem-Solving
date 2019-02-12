1b8afde8baf337bef72db8c26cd55a85cad596d3
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-02-13 02:15:00 +0600

    Add solution for problem `B - Squares and Segments` Link: https://codeforces.com/contest/1099/problem/B Tags: binary search, constructive algorithms, math, *1100 Ref: https://codeforces.com/contest/1099/submission/49844120

--- codeforces/1099/B.cpp
+++ codeforces/1099/B.cpp
@@ -7,11 +7,16 @@ int main()
 {
    int m;
    cin>>m;
-
+if(m==1)
+    cout<<2<<endl;
+   else if(m==2)
+    cout<<3<<endl;
+ if(m>2)
+ {
      double sura;
      sura=sqrt(m)*2*1.0;
      cout<<ceil(sura)<<endl;
-
+ }
 
 
 
