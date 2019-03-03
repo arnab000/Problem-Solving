02b481637d76379cade8bf7024fe4bb43d895806
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-03-03 23:29:00 +0600

    Add solution for problem `A - Technogoblet of Fire` Link: https://codeforces.com/contest/1121/problem/A Tags: implementation, sortings, *1100 Ref: https://codeforces.com/contest/1121/submission/50756488

--- codeforces/1121/A.cpp
+++ codeforces/1121/A.cpp
@@ -1,28 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int power[105],school[105];
 int main()
 {
+    int i,j,k,l,n,m,v1,v2=0,v3,v4,o,t;
+    vector<int>po;
+     vector<int>po1;
+    vector<int>sch;
+    vector<int>cho;
+    cin>>n>>m>>k;
+    for(i=0;i<n;i++)
+    {
+        cin>>v1;
+        po.push_back(v1);
+        po1.push_back(v1);
+        power[v1]=i;
 
-    int arr[102]={0},brr[102]={0},crr[102]={0};
-    int a,b,c,d=0;
-    scanf("%d %d %d",&a,&b,&c);
-    for(int i=1; i<= a*2; i++){
-        if(i <= a)
-            scanf("%d",&arr[i]);
-        else{
-            int x;
-            scanf("%d",&x);
-            crr[i-a]=x;
-            if(brr[x] < arr[i-a])
-                brr[x]=arr[i-a];
-        }
     }
-     for(int i=0; i < c; i++){
-        int x;
-        scanf("%d",&x);
-        if(brr[crr[x]] > arr[x])
-            d++;
-     }
-     printf("%d\n",d);
+    for(i=0;i<n;i++)
+    {
+        cin>>v3;
+        sch.push_back(v3);
+        school[po[i]]=v3;
+
+    }
+    sort(po.begin(),po.end(),greater<int>());
+    v2=0;
+     for(i=1;i<=k;i++)
+    {
+        cin>>v4;
+       o =sch[v4-1];
+       t=po1[v4-1];
+      // cout<<t<<" "<<o<<endl;
+
+j=0;
+        while(po[j]!=t)
+
+        {
+
+
+            if(o==school[po[j]])
+            {
+                v2++;
+                break;
+            }
+         j++;
+
+
+        }
+        //cout<<v2<<endl;
+
+    }
+    cout<<v2<<endl;
+
+
+
 }
