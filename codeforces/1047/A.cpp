a167c4f5a2b34b2939306d5c8ede21c998faef6a
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-03-26 01:37:00 +0600

    Add solution for problem `A - Little C Loves 3 I` Link: https://codeforces.com/contest/1047/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1047/submission/51838296

---
+++ codeforces/1047/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+ long long a;
+ cin>>a;
+ if(a%3==0)
+ {
+     cout<<a-2<<" "<<1<<" "<<1<<endl;
+ }
+ else{
+    cout<<a-3<<" "<<2<<" "<<1<<endl;
+ }
+}
