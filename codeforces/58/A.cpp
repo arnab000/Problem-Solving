02878bde879546dc44b90adac678ccc929622f86
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-03-26 18:19:00 +0600

    Add solution for problem `A - Chat room` Link: https://codeforces.com/contest/58/problem/A Tags: greedy, strings, *1000 Ref: https://codeforces.com/contest/58/submission/51858876

---
+++ codeforces/58/A.cpp
@@ -0,0 +1,54 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int lcs( string X, string  Y, int m, int n )
+{
+int L[m+1][n+1];
+int i, j;
+
+for (i=0; i<=m; i++)
+{
+	for (j=0; j<=n; j++)
+	{
+	if (i == 0 || j == 0)
+		L[i][j] = 0;
+
+	else if (X[i-1] == Y[j-1])
+		L[i][j] = L[i-1][j-1] + 1;
+
+	else
+		L[i][j] = max(L[i-1][j], L[i][j-1]);
+	}
+}
+
+
+
+return L[m][n];
+}
+
+
+
+int main()
+{
+string s,s1;
+s="hello";
+
+  getline(cin,s1);
+
+ int n= lcs( s,s1,s.size(), s1.size() );
+ if(n==s.size())
+    cout<<"YES"<<endl;
+ else
+    cout<<"NO"<<endl;
+
+
+
+
+
+
+}
+
