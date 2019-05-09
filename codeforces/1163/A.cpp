5cbc81f2ea54f97b430f80ee18908fa9ede35c00
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-05-09 21:21:00 +0600

    Add solution for problem `A - Eating Soup` Link: https://codeforces.com/contest/1163/problem/A Tags: greedy, math, *900 Ref: https://codeforces.com/contest/1163/submission/53913005

---
+++ codeforces/1163/A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+long long n,m,i,j,k,l;
+cin>>n>>m;
+if(m==0)
+{
+    cout<<1<<endl;
+    return 0;
+
+
+}
+cout<<min(n-m,m)<<endl;
+
+
+
+
+
+}
