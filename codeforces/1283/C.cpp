1c5560293bd2b74006d2704aea8dec1ec4f85ccb
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-12-29 03:43:00 +0600

    Add solution for problem `C - Friends and Gifts` Link: https://codeforces.com/contest/1283/problem/C Tags: constructive algorithms, data structures, math, *1500 Ref: https://codeforces.com/contest/1283/submission/67846114

--- codeforces/1283/C.cpp
+++ codeforces/1283/C.cpp
@@ -1,45 +1,70 @@
 #include <bits/stdc++.h>
-#pragma GCC optimize("O2")
-#define all(c) c.begin(), c.end()
-#define pb push_back
-#define ll long long
-const int MAXN = 2e5 + 5;
-const int mod = 1e9 + 7;
+/*#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>*/
  
+#define pll              pair <long long,long long>
+#define pb               push_back
+#define ll               long long
+#define ff               first
+#define ss               second
+#define D(x)             cerr<<#x " = "<<(x)<<endl
+#define vll              vector <ll>
+#define IOS              ios_base::sync_with_stdio(0); cin.tie(NULL);
+#define endl             '\n'   
 using namespace std;
- 
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int n;
-  cin >> n;
-  int f[MAXN];
-  set<int> se;
-  for (int i = 1; i <= n; i++)
-    se.insert(i);
-  for (int i = 1; i <= n; i++) {
-    cin >> f[i];
-    if (f[i] != 0)
-      se.erase(f[i]);
-  }
-  int j = 0;
-  for (int i = 1; i <= n; i++) {
-    if (f[i] != 0) continue;
-    if (i == *se.begin() && se.size() == 1) {
-      f[i] = f[j];
-      f[j] = i;
-      continue;
-    }
-    j = i;
-    if (i != *se.begin()) {
-      f[i] = *se.begin();
-      se.erase(se.begin());
-    } else {
-      f[i] = *next(se.begin());
-      se.erase(next(se.begin()));
-    }
-  }
-  for (int i = 1; i <= n; i++)
-    cout << f[i] << ' ';
-  return 0;
+int main()
+{
+    IOS
+ 	// freopen("input.txt","r",stdin);
+ 	// freopen("output.txt","w",stdout);
+ 	// freopen("error.txt","w",stderr);
+ 	ll n;
+ 	cin>>n;
+ 	ll f[n];
+ 	set<ll>rec;
+ 	for(ll i=0;i<n;i++)
+ 	{
+ 		rec.insert(i);
+ 	}
+ 	for(ll i=0;i<n;i++)
+ 	{
+ 		cin>>f[i];
+ 		f[i]--;		
+ 		if(f[i]!=-1)
+ 		{
+ 		rec.erase(rec.find(f[i]));
+ 		}
+ 	}	
+ 	for(ll i=0;i<n;i++)
+ 	{
+ 		if(f[i]==-1 && rec.find(i)!=rec.end())
+ 		{	
+ 			ll top=*rec.begin();
+ 			if(top==i)
+ 			{
+ 				top=*rec.rbegin();
+ 			}
+ 			rec.erase(top);
+ 			f[i]=top;
+ 		}
+ 	}
+ 	for(ll i=0;i<n;i++)
+ 	{
+ 		if(f[i]==-1)
+ 		{	
+ 			ll top=*rec.begin();
+ 			if(top==i)
+ 			{
+ 				top=*rec.rbegin();
+ 			}
+ 			rec.erase(top);
+ 			cout<<top+1<<" ";
+ 		}
+ 		else
+ 		{
+ 			cout<<f[i]+1<<" ";
+ 		}
+ 	}
+ 	cout<<endl;
+    return 0;
 }
