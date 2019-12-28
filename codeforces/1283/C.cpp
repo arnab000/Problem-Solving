7d7f8ef5399377cd8a6117d0ce5f77a9d20940f4
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-12-29 03:44:00 +0600

    Add solution for problem `C - Friends and Gifts` Link: https://codeforces.com/contest/1283/problem/C Tags: constructive algorithms, data structures, math, *1500 Ref: https://codeforces.com/contest/1283/submission/67846160

--- codeforces/1283/C.cpp
+++ codeforces/1283/C.cpp
@@ -1,71 +1,45 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize("O2")
+#define all(c) c.begin(), c.end()
+#define pb push_back
+#define ll long long
+const int MAXN = 2e5 + 5;
+const int mod = 1e9 + 7;
+ 
 using namespace std;
-#define endl "\n"
-#define mod 1000000007
-#define loopitr(i, x) for(auto i=x.begin(); i!=x.end(); i++)
-#define loopitrev(i, x) for(auto i=x.rbegin(); i!=x.rend(); i++)
-#define bug " BUG\n"
-using ll=long long;
-using vll=vector<ll>;
-using vpp=vector<pair<ll, ll> >;
  
 int main() {
-    ll n, t1, t2, t3;
-    vll arr, same, rem;
-    set<int> left;
- 
-    cin >> n;
-    vll sett(n+5, 0);
-    for(int i = 0; i < n; i++) {
-        cin >> t1;
-        arr.push_back(t1);
-        sett[t1] = 1;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  int f[MAXN];
+  set<int> se;
+  for (int i = 1; i <= n; i++)
+    se.insert(i);
+  for (int i = 1; i <= n; i++) {
+    cin >> f[i];
+    if (f[i] != 0)
+      se.erase(f[i]);
+  }
+  int j = 0;
+  for (int i = 1; i <= n; i++) {
+    if (f[i] != 0) continue;
+    if (i == *se.begin() && se.size() == 1) {
+      f[i] = f[j];
+      f[j] = i;
+      continue;
     }
-    for(int i = 1; i <= n; i++) {
-        if(sett[i] == 0) left.insert(i);
+    j = i;
+    if (i != *se.begin()) {
+      f[i] = *se.begin();
+      se.erase(se.begin());
+    } else {
+      f[i] = *next(se.begin());
+      se.erase(next(se.begin()));
     }
-    loopitr(i, left) {
-        if(arr[(*i)-1] == 0) {
-            same.push_back(*i);
-            //left.erase(*i);
-            rem.push_back(*i);
-        }
-    }
-    loopitr(i, rem) {
-        left.erase(*i);
-        // cout << *i << bug;
-    }
-    // loopitr(i, left) cout << *i << ' ';
-    // cout << bug;
-    // loopitr(i, same) cout << *i << ' ';
-    for(int i = 0; i < same.size(); i += 2) {
-        if(i + 1 < same.size()) {
-        arr[same[i]-1] = same[i+1];
-        arr[same[i+1]-1] = same[i];}
-    }
-    if(left.size()!=0) {
-        if(same.size()&1) {
-            arr[same.back()-1] = *left.begin();
-            left.erase(*left.begin());
-            for(int i = 0; i < n; i++) {
-                if(arr[i] == 0) {
-                    arr[i] = same.back();
-                    break;
-                }
-            }
-        }
-        auto j = left.begin();
-        for(int i = 0; i < n; i++) {
-            if(!arr[i]) {
-                arr[i] = *j;
-                j++;
-            }
-        }
-    }
-    else if(same.size()&1) {
-        arr[same.back()-1] = same[1];
-        arr[same[1]-1] = same[0];
-        arr[same[0]-1] = same.back();
-    }
-    loopitr(i, arr) cout << *i << ' ';
+  }
+  for (int i = 1; i <= n; i++)
+    cout << f[i] << ' ';
+  return 0;
 }
