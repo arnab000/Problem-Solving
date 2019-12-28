7220a92b49ae8740c5835dc09cca2d62805bf35e
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-12-29 04:06:00 +0600

    Add solution for problem `C - Friends and Gifts` Link: https://codeforces.com/contest/1283/problem/C Tags: constructive algorithms, data structures, math, *1500 Ref: https://codeforces.com/contest/1283/submission/67846671

--- codeforces/1283/C.cpp
+++ codeforces/1283/C.cpp
@@ -1,57 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define ull unsigned long long
-void bst(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-}
-int arr[(int)2e5 + 100];
-bool pol[(int)2e5 + 100];
-int ans[(int)2e5 + 100];
-bool pod[(int)2e5 + 100];
-int main() {
-	bst();
-	int n;
-	cin >> n;
-	for(int i = 1; i <= n;i++){
-	cin >> arr[i];
-	if(arr[i] != 0){
-	pol[arr[i]] = 1;
-	pod[i] = 1;
-	}
-	}
-	int cnt = 0;
-	for(int i = 1;i <= n;i++){
-		if(pol[i] == 0 && pod[i] == 1)ans[cnt] = i,cnt++;
-	}
-	int k = 0;
-	for(int i = 1;i <= n;i++){
-	if(pod[i] == 0 && pol[i] == 0 && k != cnt ){
-		arr[i] = ans[k];
-		k++;
-		pod[i] = 1;
-		pol[arr[i]] = 1;
-	}
-}
-cnt = 0;
-	for(int i = 1;i <= n;i++){
-		if(pol[i] == 0)ans[cnt] = i,cnt++;
-	}
-k = 0;
-for(int i = 1;i <= n;i++){
-	if(arr[i] == 0){
-		if(pod[ans[k + 1]] == 0 && cnt - k == 2)swap(ans[k], ans[k + 1]);
-		if(i == ans[k])swap(ans[k],ans[k + 1]);
-		arr[i] = ans[k];
-		k++;
-		pol[arr[i]] = 1;
-		pod[i] = 1;
-	}
-}
-for(int i = 1;i <= n;i++){
-	cout<<arr[i]<<" ";
-}
-	return 0;
+int n,b[200005],a[200005],dem,c[200005],kt[200005],m,pre;
+int main()
+{
+   // freopen("a.inp","r",stdin);
+   // freopen("a.out","w",stdout);
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+        kt[a[i]]=1;
+    }
+    for (int i=1;i<=n;i++)
+    {
+        if (kt[i]==0)
+        {
+            dem++;
+            b[dem]=i;
+        }
+    }
+    m=dem;
+    dem=0;
+    for (int i=1;i<=n;i++)
+    {
+        if (a[i]!=0);
+        else {
+            dem++;
+        if (b[dem]==i && m!=dem)
+        {
+            m++;
+            b[m]=i;
+            dem++;
+        }
+        else if (m==dem && b[dem]==i)
+        {
+          //  cout<<a[2]<<endl;
+            a[i]=a[pre];
+            a[pre]=b[dem];
+ 
+            break;
+        }
+        pre=i;
+        //cout<<b[dem]<<endl;
+                a[i]=b[dem];
+        }
+    }
+    for (int i=1;i<=n;i++) cout<<a[i]<<' ';
 }
