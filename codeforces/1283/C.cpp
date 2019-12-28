7e735bb2fcc81f47948bd86c3c6ea908d5d50e04
Arnab Saha <arnab.saintjoseph@gmail.com>
2019-12-29 03:39:00 +0600

    Add solution for problem `C - Friends and Gifts` Link: https://codeforces.com/contest/1283/problem/C Tags: constructive algorithms, data structures, math, *1500 Ref: https://codeforces.com/contest/1283/submission/67845998

--- codeforces/1283/C.cpp
+++ codeforces/1283/C.cpp
@@ -1,70 +1,70 @@
 #include <bits/stdc++.h>
-/*#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>*/
- 
-#define pll              pair <long long,long long>
-#define pb               push_back
-#define ll               long long
-#define ff               first
-#define ss               second
-#define D(x)             cerr<<#x " = "<<(x)<<endl
-#define vll              vector <ll>
-#define IOS              ios_base::sync_with_stdio(0); cin.tie(NULL);
-#define endl             '\n'   
+#define inl(x) scanf("%lld",&x)
+#define in(x) scanf("%d",&x)
+#define ll long long
+#define mp(x,y) make_pair(x,y)
+#define mxx 100000000000000000
+#define M 200005
+#define pi 2*acos(0.0)
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 using namespace std;
+ 
+ 
+bool dise[M],paise[M];
+int solution[M];
+std::vector<int> dibe,pabe;
+ 
+void sort()
+{
+    for(int i=0;i<dibe.size();i++)
+    {
+        if(dibe[i]==pabe[i])
+        {
+            if(i==dibe.size()-1)
+            {
+                swap(pabe[i],pabe[i-1]);
+            }
+            else
+                swap(pabe[i],pabe[i+1]);
+        }
+    }
+}
+ 
+ 
 int main()
 {
-    IOS
- 	// freopen("input.txt","r",stdin);
- 	// freopen("output.txt","w",stdout);
- 	// freopen("error.txt","w",stderr);
- 	ll n;
- 	cin>>n;
- 	ll f[n];
- 	set<ll>rec;
- 	for(ll i=0;i<n;i++)
- 	{
- 		rec.insert(i);
- 	}
- 	for(ll i=0;i<n;i++)
- 	{
- 		cin>>f[i];
- 		f[i]--;		
- 		if(f[i]!=-1)
- 		{
- 		rec.erase(rec.find(f[i]));
- 		}
- 	}	
- 	for(ll i=0;i<n;i++)
- 	{
- 		if(f[i]==-1 && rec.find(i)!=rec.end())
- 		{	
- 			ll top=*rec.begin();
- 			if(top==i)
- 			{
- 				top=*rec.rbegin();
- 			}
- 			rec.erase(top);
- 			f[i]=top;
- 		}
- 	}
- 	for(ll i=0;i<n;i++)
- 	{
- 		if(f[i]==-1)
- 		{	
- 			ll top=*rec.begin();
- 			if(top==i)
- 			{
- 				top=*rec.rbegin();
- 			}
- 			rec.erase(top);
- 			cout<<top+1<<" ";
- 		}
- 		else
- 		{
- 			cout<<f[i]+1<<" ";
- 		}
- 	}
- 	cout<<endl;
+ 
+    int n,x;
+    cin >> n ;
+    for(int i=1;i<=n;i++)
+    {
+        in(x);
+        if(x)
+        {
+            dise[i] = 1;
+            paise[x] = 1;
+            solution[i] = x;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(!dise[i])
+            dibe.push_back(i);
+        
+        if(!paise[i])
+            pabe.push_back(i);
+    }
+    sort();
+    for(int i=0;i<dibe.size();i++)
+    {
+        //if(dibe[i]==pabe[i])
+ 
+        solution[dibe[i]] = pabe[i];
+    }
+    for(int i=1;i<=n;i++)
+        printf("%d%c",solution[i] , " \n"[i==n] );
     return 0;
 }
