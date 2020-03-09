a64c44f85c5e66194e5b519afc18b10a67afc619
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 02:49:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72855438

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,44 +1,78 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define mp make_pair
+#define INF (int)1e9
+#define cBits(x) __builtin_popcount(x)
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) a/gcd(a,b)*b
+#define FOR(i,a,b) for (int i=a;i<b;i++)
+#define FORr(i,a,b) for (int i=a;i>=b;i--)
+#define MAX3(a,b,c) max(a,max(b,c))
+#define MIN3(a,b,c) min(a,min(b,c))
+#define eachTest int t; cin >> t; while(t--) solve();
+ 
 using namespace std;
-int main() {
-	int t;
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		int n, k;
-		cin >> n >> k;
-		vector<long long> a(n);
-		for (int j = 0; j < n; j++) 
-			cin >> a[j];
-		vector<long long> st1;
-		vector<int> st2;
-		st1.push_back(1);
-		st2.push_back(0);
-		for (int j = 1; j < 64; j++) {
-			if (st1[j - 1] * k > 10000000000000000)
-				break;
-			st2.push_back(0);
-			st1.push_back(st1[j - 1] * k);
+ 
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef pair<ll,ll> PLL;
+typedef pair<int,int> PII;
+ 
+const int MOD=1000000007;
+ 
+void solve() {
+	ll n, k;
+	cin >> n >> k;
+	vector <ll> a(n);
+	// set <ll> forEach[n];
+	set <ll> uniqReq;
+	ll x;
+	FOR(i,0,n) {
+		cin >> a[i];
+	}
+	FOR(i,0,n) {
+		// cin >> a[i];
+		if (a[i]==0) {
+			continue;
 		}
-		for (int j = 0; j < n; j++) {
-			for (int l = st1.size()-1; l >= 0; l--) {
-				if (a[j] >= st1[l]) {
-					if (st2[l] == 0) {
-						st2[l] = 1;
-						a[j] -= st1[l];
-					}
+		x = a[i];
+		ll p = 0;
+		bool flag = false;
+		while(x) {
+			ll rem = x%k;
+			if (rem > 1) {
+				flag = true;
+				cout << "NO\n";
+				return;
+			}
+			if (rem == 1) {
+				if (uniqReq.find(p) != uniqReq.end()) {
+					cout << "NO\n";
+					return;
+				}
+				else {
+					uniqReq.insert(p);
 				}
 			}
-		}
-		for (int j = 0; j < n; j++) {
-			if (a[j] != 0) {
-				cout << "NO" << endl;
-				break;
-			}
-			if (j == n - 1)
-				cout << "YES" << endl;
+			p++;
+			x /= k;
 		}
 	}
+	cout << "YES\n";
+}
+ 
+int main() {
+ 
+	fastio;
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+ 
+	eachTest
+ 
+	
+ 
 	return 0;
 }
