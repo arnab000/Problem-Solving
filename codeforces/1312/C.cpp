a8e2adca751139958019c34dbb95e06c59febb51
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 02:44:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72855241

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,70 +1,48 @@
-#include <bits/stdc++.h>
-#define ll long long 
+#include<bits/stdc++.h>
+ 
 using namespace std;
+#define ll long long
+ll num[35];
+bool vis[105];
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	int t;
-	cin>>t;
-	while(t--){
-		ll n, k;
-		cin>>n>>k;
-		vector<ll> arr;
-		for(int i = 0; i < n; i++){
-			ll a;
-			cin>>a;
-			arr.push_back(a);
-		}
-		vector<ll> pre;
-		ll at = 1;
-		pre.push_back(at);
-		for(int i = 1; i <= 64;i++){
-			at *= k;
-			if(at < 0)
-				break;
-			pre.push_back(at);
-		}
-		sort(pre.begin(),pre.end());
-		sort(arr.begin(),arr.end());
-		bool okay = true;
-		bool used[1000];
-		memset(used,false,sizeof(used));
-		for(int i = 0; i < n; i++){
-				bool ok = true;
-				while(arr[i] > 0){
-					ll pos = upper_bound(pre.begin(), pre.end(), arr[i]) - pre.begin();
-					ll value = pre[pos];
-					value /= k;
-					pos--;
-					//cout<<arr[i]<<" ";
-					if(pos < 0)
-					{
-						pos = 0;
-						value = 1;
-					}
-					if(used[pos])
-					{
-						ok = false;
-						break;
-					}
-					else
-					{
-						arr[i] -= pre[pos];
-						used[pos] = true;
-					}
-				}
-				if(!ok)
-				{
-					okay = false;
-					break;
-				}
-		}
-		
-		if(okay)
-			puts("YES");
-		else
-			puts("NO");
-	}
-	return 0;
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        memset(vis,0,sizeof(vis));
+        ll n,k;
+        cin >> n >> k;
+        for(int i=0;i<n;i++)
+            scanf("%lld",&num[i]);
+        int f=1;
+        for(int i=0;i<n && f;i++)
+        {
+            ll t=1,cnt=0;
+            while(t<num[i]){t*=k;cnt++;}
+            for(int j=cnt;j>=0;j--,t/=k)
+            {
+                if(t<=num[i])
+                {
+                    num[i]-=t;
+                    if(!vis[j])
+                    {
+                        vis[j]=1;
+                    }
+                    else
+                    {
+                        f=0;
+                        break;
+                    }
+                }
+            }
+            if(num[i])
+                f=0;
+        }
+        if(f)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
 }
