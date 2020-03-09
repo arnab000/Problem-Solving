0019587d8aa013b4bf4b14f4803bcc990ad471c5
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 02:27:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72854497

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,48 +1,85 @@
-#include<bits/stdc++.h>
- 
-using namespace std;
+#include <bits/stdc++.h>
 #define ll long long
-ll num[35];
-bool vis[105];
+#define fr(i,p,n) for(int i=p;i<n;i++)
+#define fr1(i,p,n) for(int i=p;i>=n;i--)
+#define pb push_back
+#include <stdio.h>
+#define PI 3.14159265358979323846
+#define MOD 1000000007
+#define precise cout << std::setprecision(10) << std::fixed;
+using namespace std;
+ 
+bool f=false;
+map<ll,ll> mp;
+void tt(ll a,ll k)
+{
+    ll c=0,d=0;
+    if(a==1) mp[0]++;
+    else if(a%k==0)
+    {
+        while(1)
+        {
+            while(a%k==0)
+            {
+                a=a/k;
+                c++;
+            }
+            if(a==1) {mp[c]++;break;}
+            else if((a-1)%k==0) {mp[c]++;a--;}
+            else {f=true;break;}
+        }
+    }
+    else if((a-1)%k==0)
+    {
+        a--;
+        mp[0]++;
+        while(1)
+        {
+            while(a%k==0)
+            {
+                a=a/k;
+                c++;
+            }
+            if(a==1) {mp[c]++;break;}
+            else if((a-1)%k==0) {a--;}
+            else {f=true;break;}
+        }
+    }
+    else f=true;
+}
 int main()
 {
-    int T;
-    cin >> T;
-    while(T--)
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    ios::sync_with_stdio(0);
+    ll t;
+    cin>>t;
+    while(t--)
     {
-        memset(vis,0,sizeof(vis));
         ll n,k;
-        cin >> n >> k;
-        for(int i=0;i<n;i++)
-            scanf("%lld",&num[i]);
-        int f=1;
-        for(int i=0;i<n && f;i++)
+        cin>>n>>k;
+        ll a[n];
+        fr(i,0,n) cin>>a[i];
+        f=false;
+        mp.clear();
+        fr(i,0,n)
         {
-            ll t=1,cnt=0;
-            while(t<num[i]){t*=k;cnt++;}
-            for(int j=cnt;j>=0;j--,t/=k)
-            {
-                if(t<=num[i])
-                {
-                    num[i]-=t;
-                    if(!vis[j])
-                    {
-                        vis[j]=1;
-                    }
-                    else
-                    {
-                        f=0;
-                        break;
-                    }
-                }
-            }
-            if(num[i])
-                f=0;
+            if(a[i]==0) continue;
+            tt(a[i],k);
+            if(f==true) break;
         }
-        if(f)
-            printf("YES\n");
+        if(f) cout<<"NO"<<endl;
         else
-            printf("NO\n");
+        {
+            for (ll i = 0; i <= 60; i++)
+            {
+                //cout<<mp[i]<<" ";
+                if(mp[i]>1) {f=true;break;}
+            }
+            //cout<<endl;
+            if(f) cout<<"NO"<<endl;
+            else cout<<"YES"<<endl;
+        }
     }
     return 0;
 }
