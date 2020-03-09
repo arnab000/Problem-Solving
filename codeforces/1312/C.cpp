e7af3c2b7ad7fa2ab5d0bcf878b71169cd14b2ef
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:18:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72850844

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,60 +1,76 @@
-#include <bits/stdc++.h>
-#define ll long long  
-#define inf (ll)1000000000000000
-#define inf1 (ll)1000000000000000
-#define MOD 1000000007
-#define maxn 400010
-#define pb push_back
-#define pll pair<ll,ll>
-#define vi vector<ll>
-#define vvi vector<vector<ll> >
-#define vii vector<pll> 
-#define vvpll vector<vector<pll> >
-#define vpll vector<pll> 
+﻿#include<iostream>
+ 
+#include<bits/stdc++.h>
  
 using namespace std;
+#define SWS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+typedef long long ll;
  
-int main() 
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ll t;
-	cin>>t;
-	while(t--)
-	{
-		ll n,k;
-		cin>>n>>k;
-		ll a[n],powk[64];
-		powk[0]=1;
-		for(int i=1;i<64;i++)
-		{
-			powk[i]=powk[i-1]*k;
+const int MOD = (int)1e9 + 7;
+ 
+ 
+ 
+ 
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		long long sizee;
+		ll k;
+ 
+		bool Good = true;
+		cin >> sizee >>k;
+		vector<ll>Mas(sizee);
+		vector<bool>Dt(100 , true);
+ 
+ 
+		for (int i = 0; i < sizee; i++) {
+			cin >> Mas[i];
 		}
-		map<ll,ll> mp;
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-			ll temp=a[i],cnt=0;
-			while(temp>0)
-			{
-				mp[powk[cnt]]+=temp%k;
-				temp/=k;
-				cnt++;
+ 
+		for (int i = 0; i < sizee; i++) {
+ 
+ 
+			ll temp = Mas[i];
+			vector<bool>Temp(100, true);
+			int its = 0;
+ 
+			while (temp > 1) {
+ 
+				if (temp % k > 1) {
+					Good = false;
+					break;
+				}
+				else if (temp % k == 1) {
+					Temp[its] = false;
+				}
+				its++;
+				temp = temp / k;
 			}
-		}
-		int flag=0;
-		for(int i=0;i<64;i++)
-		{
-			if(mp[powk[i]]>=2)
-			{
-				cout<<"NO\n";
-				flag=1;
-				break;
+			if( Mas[i] != 0)
+			Temp[its] = false;
+		
+			for (int i = 0; i <=its; i++) {
+				if (Temp[ i ] == false) {
+					if (Dt[i] == false) {
+						Good = false;
+						break;
+					}
+					else {
+						Dt[i] = false;
+					}
+				}
 			}
+ 
+			
 		}
-		if(flag==0)
-		{
-			cout<<"YES\n";
+		if (Good == false) {
+			cout << "NO" << endl;
 		}
+		else { cout << "YES" << endl; }
+ 
+ 
 	}
+	//	system("pause");
+	return 0;
 }
