2b115fce5faf6d4ecff7e98115a08e73cde31c47
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:52:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72852832

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,45 +1,73 @@
-#include<bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
-#define Jewel ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-int main()
-{
-    Jewel;
-    int q;
-    cin>>q;
-    while(q--)
-    {
-        long long int n, k, i, a, d, ind;
-        cin>>n>>k;
-        set<long long>s;
-        s.clear();
-        bool ok = true;
-        for(i=0; i<n; i++)
-        {
-            cin>>a;
-            ind=0;
-            while(a>0 && ok)
-            {
-                d = a%k;
-                a = a/k;
  
-                if(d>1)
-                    ok = false;
-                if(d)
-                {
-                    if(s.count(ind))
-                        ok = false;
-                    else
-                        s.insert(ind);
-                }
-                ind++;
-            }
+using ll = long long;
  
-        }
-        if(ok)
-            cout<<"YES"<<endl;
-        else
-            cout<<"NO"<<endl;
- 
+ll nearest(ll x, int base){
+  int ret = 0;
+  for(int i = 0; i < 60; i++){
+    if(pow(base, i) > x){
+      ret = i - 1;
+      break;
     }
-    return 0;
+  }
+  return ret;
+}
+ 
+bool bad = 0;
+ 
+vector<int> get(ll x, int base){
+  vector<int> v;
+  while(x >= base){
+    int xx = nearest(x, base);
+    v.push_back(xx);
+    x -= pow(base, xx);
+  }
+  if(x == 1){
+    v.push_back(0);
+    x--;
+  }
+  if(x != 0){
+    bad = 1;
+  }
+  return v;
+}
+ 
+int main(int argc, char const *argv[])
+{
+//  freopen("in.txt", "r", stdin);
+  int t;
+  scanf("%d", &t); 
+  while(t--){
+    bad = 0;
+    int n, base;
+    scanf("%d %d", &n, &base);
+    vector<ll> a(n);
+    vector<int> cnt(61);
+    for(int i = 0; i < n; i++){
+      scanf("%lld", &a[i]);
+      if(a[i] == 0)continue;
+      vector<int> ret = get(a[i], base);
+      for(auto j : ret){
+        cnt[j]++;
+      }
+    }
+    if(bad == 1){
+      printf("NO\n");
+    }else {
+      for(int i = 0; i <= 60; i++){
+        if(cnt[i] > 1){
+          bad = 1;
+          break;
+        }
+      }
+      if(bad){
+        printf("NO\n");
+      }else {
+        printf("YES\n");
+      }
+    }
+  }
+ 
+  return 0;
 }
