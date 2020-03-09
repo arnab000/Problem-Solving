e54db65ccf7638f7bf3c68bc97688fa6d6c3a535
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:48:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72852631

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,147 +1,77 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+ 
 using namespace std;
  
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
  
-#define endl '\n'
-#define ll long long
-#define sc(a) scanf("%d",&a)
-#define scl(a) scanf("%lld",&a)
-#define mem(a , v)  memset(a , v , sizeof(a))
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
- 
- 
-const int MAX = 1e5+55;
-const int INF = 1e9+77;
-const int MOD = 1e9+7;
-const double PI = acos(-1.0);
-vector<int> months = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
- 
- 
-ll power(ll x,ll y){
-         if(y==0)
-         return 1;
-         ll s=power(x,y/2);
-         s*=s;
-         if(y%2==1)
-         s*=x;
-         return s;
-}
- 
-ll gcd (ll a, ll b) {
-return b ? gcd (b, a % b) : a;
-}
- 
-ll fac(ll x){
-         ll p=1;
-         for(ll i=2;i<=x;i++){
-         p*=i;
-         }
-         return p;
-}
- 
+#define PI (2*acos(0.0))
+#define eps 1e-9
+#define pb push_back
+#define endl "\n"
+#define watch(x) cout << (#x) << " is " << (x) << endl;
+#define show(v) for(int fi = 0; fi < v.size(); fi++) cout << v[fi] << " "; cout << endl;
+#define showpair(v) for(int fi = 0; fi < v.size(); fi++) cout << v[fi].first << " " << v[fi].second << endl;
+#define ff first
+#define ss second
+#define fu cout << "lol" << endl;
+#define precision(n) cout << fixed << setprecision(n);
+#define lb lower_bound
+#define up upper_bound
+#define vscan for(i = 0;i<n;i++){cin>>in; v.pb(in);}
+#define all(a) a.begin(), a.end()
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define mem(a,val) memset(a,val,sizeof(a))
+#define loop(i,n) for(i = 0; i < n; i++)
+#define TC() ull T; cin>>T; while(T--)
+#define IN(x) {scanf("%d",&x);}
+#define LL(x) {scanf("%lld",&x);}
+#define CC(x) {scanf("%c",&x);}
+#define pfl(x) printf("%d\n",x)
+#define pfll(x) printf("%lld\n",x)
+#define newl puts("")
+#define space printf(" ")
+#define MOD 1000000007
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
  
  
- 
- 
- 
-int main(){
- 
-ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
- 
-//freopen("input.txt" , "r" , stdin);
-//freopen("output.txt" , "w" , stdout);
- 
- 
-    int t;
-    cin>>t;
- 
- 
- 
- 
-   while(t--){
- 
-       ll n,k;
-       cin>>n>>k;
- 
-       ll x[n];
-       ll max1=0;
-       for(int i=0;i<n;i++){
-        cin>>x[i];
-        max1=max(x[i],max1);
-       }
- 
-       
- 
-       ll y[MAX];
-       bool is[MAX];
- 
-       for(int i=0;i<MAX;i++){
-        y[i]=0;
-        is[i]=0;
-       }
- 
-       int pos=0;
-       for(ll i=0;i<MAX;i++){
-        y[i]=power(k,i);
-        pos=i+1;
-        if(y[i]>max1){
-            break;
+int main()
+{
+    int i = 0, j = 0, cs = 0, in;
+    TC(){
+        int n, k; cin>>n>>k;
+        vll v;
+        for(i = 0; i < n; i++){
+            ll in; cin>>in; v.pb(in);
         }
-       }
- 
-       
- 
- 
- 
- 
- 
- 
-       bool t=1;
-       for(int i=0;i<n;i++){
-         if(x[i]==0){continue;}
-         bool h[MAX];
-         for(int j=0;j<pos;j++){
-            h[j]=0;
-         }
- 
-         ll cur=x[i];
-         for(int j=pos;j>=0;j--){
-            if(cur>=y[j]&&!is[j]){
-              cur-=y[j];
-              h[j]=1;
-            }
-         }
- 
-         if(cur==0){
-            for(int j=0;j<pos;j++){
-                if(h[j]){
-                    is[j]=1;
+        bool vis[65]; mem(vis,false);
+        bool f = 0;
+        for(i = 0; i < v.size(); i++){
+            ll para = v[i];
+            while(para > 0){
+                int cnt = 0;
+                ll p = 1;
+                ll lol = para;
+                while(lol % k == 0){
+                    lol /= k;
+                    p *= k;
+                    cnt++;
                 }
+                if(vis[cnt]){
+                    f = 1;
+                    break;
+                }
+                vis[cnt] = true;
+                para -= p;
             }
-         }
-         else{
-            t=0;
-            break;
-         }
- 
- 
-       }
-       
-       
-       
- 
-       if(t){
-        cout<<"YES"<<endl;
-       }
-       else{
-        cout<<"NO"<<endl;
-       }
- 
- 
-   }
- 
- 
-     return 0;
- 
+        }
+        if(f) cout << "NO" << endl;
+        else cout << "YES" <<  endl;
+    }
+    return 0;
 }
- 
