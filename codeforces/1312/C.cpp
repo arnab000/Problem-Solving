36ce7bbfca6ab4c876b5e4e74dc67e3511b9f728
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 02:04:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72853469

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,81 +1,83 @@
-#include <bits/stdc++.h>
- 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef unsigned short int us;
- 
-#define forn(i, n) for (int i = 0; i < int(n); ++i)
- 
+#include<bits/stdc++.h>
 using namespace std;
+ 
  
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
  
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n, k;
-        cin >> n >> k;
-        vector<int> u(70);
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    long long int t;
+    cin>>t;
+    while(t--)
+	{
+		
+		long long int n,k;
+		cin>>n>>k;
+		long long int a[n];
+		int flag = 0;
+		map<long long int,long long int> mp;
+		for(int i = 0;i<n;i++)
+		{
+			cin>>a[i];
+			
+		}
+		
  
-        bool no = false;
+		for(int i = 0;i<n;i++)
+		{
+			vector<long long int> v; 
+		 	long long int x;
+		 	x=a[i];
+		 	if(x==0)
+		 		continue;
+		 	//cout<<x<<": ";
+			while (x > 0)  
+			{ 
+			    v.push_back(x % k); 
+			    x = x / k; 
+			} 
+			long long int num = 0;
+			for (int i = 0; i < v.size(); i++)  
+			{ 
+			    if (v[i] == 1)  
+			    { 
+			        num = num + (long long int)pow(k,i); 
+			        if(mp[i]!=0)
+			       	{
+			       		flag=1;
+			       		break;
+			       	}
+			      	mp[i]=1;
+			    } 
+			}
  
-        for (int i = 0; i < n; ++i)
-        {
-            ll a;
-            cin >> a;
- 
-            int p = 0;
- 
-            while(a)
-            {
-                if(a % k)
-                {
-                    u[p]++;
-                    if(u[p] > 1)
-                        no = true;
-                    --a;
-                    if(a % k)
-                    {
-                        no = true;
-                        break;
-                    }
-                    else
-                    {
-                        a /= k;
-                        ++p;
-                    }
-                }
-                else
-                {
-                    a /= k;
-                    ++p;
-                }
-            }
-        }
- 
-       /* for(auto i : u)
-        {
-            cout << u[i] << ' ';
- 
-            if(u[i] > 1)
-            {
-                no = true;
-                //break;
-            }
-        }*/
- 
-        if (no)
-            cout << "NO\n";
-        else
-            cout << "YES\n";
+			//cout<<"\n";
+			if(flag==1)
+			{
+				//cout<<"a";
+				break;
+			}
+			if(num != a[i])
+			{
+				
+				flag=1;
+				break;
+			}
+		}
+		if(flag==1)
+		{
+			cout<<"NO\n";
+		}
+		else
+			cout<<"YES\n";
  
  
-    }
+ 
+ 
+		
+	}
+    return 0;
 }
- 
- 
- 
