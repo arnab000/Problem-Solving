978beb6242436fbc4e0f43d3cceca9f597870e97
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 02:00:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72853258

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,83 +1,83 @@
-#include<bits/stdc++.h>
+/*author:	Slnc*/
+	//freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+#include <bits/stdc++.h>
+#include<string>
+#include <utility>/*useful*/
+#include <iomanip>//setprecision()
+ 
+#define pb push_back
+#define pp pop_back
+#define ll long long
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define ff first
+#define ss second
+ 
 using namespace std;
  
+void NITRO(){
+	//MY CODE IS THE BEST :)
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+const ll N = 1e+17;
+const int MOD = 1e+9 + 7;
+const ll INF = 1e+18;
  
-int main()
-{
+void SOLVE() {
+    int n, k;
+    cin >> n >> k;
+    unsigned ll ma = 1;
+    set<ll> po;
+    while(ma <= N){
+        po.insert(ma);
+        ma *= k;
+    }
+    bool was[100], can = 1;
+    while(n--){
+        ll s;
+        cin >> s;
+        if(s != 0){
+            if(po.find(s) != po.end()){
+                po.erase(po.find(s));
+            }
+            else{
+                while(s > 0 && !po.empty()){
+                    if(po.find(s) != po.end()){
+                        po.erase(po.find(s));
+                        s = 0;
+                        break;
+                    }
+                    if(po.lower_bound(s) != po.end() && po.lower_bound(s) != po.begin()){
+                        set<ll>::iterator lb = po.lower_bound(s);
+                        --lb;
+                        s -= *lb;
+                        po.erase(*lb);
+                    }
+                    else break;
+                }
+                if(s != 0)can = 0;
+            }
+        }
+    }
+    if(can)cout << "YES";
+    else cout << "NO";
+    cout << "\n";
+}
  
-	#ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-    long long int t;
-    cin>>t;
-    while(t--)
-	{
-		
-		long long int n,k;
-		cin>>n>>k;
-		long long int a[n];
-		int flag = 0;
-		map<long long int,long long int> mp;
-		for(int i = 0;i<n;i++)
-		{
-			cin>>a[i];
-			
-		}
-		
- 
-		for(int i = 0;i<n;i++)
-		{
-			vector<long long int> v; 
-		 	long long int x;
-		 	x=a[i];
-		 	if(x==0)
-		 		continue;
-		 	//cout<<x<<": ";
-			while (x > 0)  
-			{ 
-			    v.push_back(x % k); 
-			    x = x / k; 
-			} 
-			long long int num = 0;
-			for (int i = 0; i < v.size(); i++)  
-			{ 
-			    if (v[i] == 1)  
-			    { 
-			        num = num + (long long int)pow(k,i); 
-			        if(mp[i]!=0)
-			       	{
-			       		flag=1;
-			       		break;
-			       	}
-			      	mp[i]=1;
-			    } 
-			}
- 
-			//cout<<"\n";
-			if(flag==1)
-			{
-				//cout<<"a";
-				break;
-			}
-			if(num != a[i])
-			{
-				
-				flag=1;
-				break;
-			}
-		}
-		if(flag==1)
-		{
-			cout<<"NO\n";
-		}
-		else
-			cout<<"YES\n";
- 
- 
- 
- 
-		
-	}
+int main() {
+    NITRO();
+    int t;
+    cin >> t;
+    while(t--) {
+        SOLVE();
+    }
     return 0;
 }
