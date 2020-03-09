fceeecd42485b6425de279a3bc79776fedca62ba
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:00:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72849667

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,126 +1,74 @@
-#include <bits/stdc++.h>
- 
+#include<bits/stdc++.h>
 using namespace std;
  
-template <typename A, typename B>
-string to_string(pair<A, B> p);
+int main()
+{
+long long test;
+cin>>test;
+while(test--)
+{
+   long long n,k;
+   cin>>n>>k;
+   vector<long long>sura;
+   bool hbe=true;
+   for(long i=0;i<n;i++)
+   {
+       long long l;
+       cin>>l;
+       sura.push_back(l);
  
-template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p);
+   }
  
-template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p);
+   if(k==1)
+   {
+           cout<<"YES"<<endl;
+           return 0;
  
-string to_string(const string& s) {
-  return '"' + s + '"';
+   }
+   map<long long ,bool>ma;
+    for(long i=0;i<n;i++)
+   {
+       vector<long long>u;
+       if(sura[i]==0)
+        continue;
+   for(long long j=0; sura[i]>0; j++)
+{
+u.push_back(sura[i]%k);
+if(sura[i]%k>1)
+{
+    hbe=false;
+    break;
 }
- 
-string to_string(const char* s) {
-  return to_string((string) s);
-}
- 
-string to_string(bool b) {
-  return (b ? "true" : "false");
-}
- 
-string to_string(vector<bool> v) {
-  bool first = true;
-  string res = "{";
-  for (int i = 0; i < static_cast<int>(v.size()); i++) {
-    if (!first) {
-      res += ", ";
-    }
-    first = false;
-    res += to_string(v[i]);
-  }
-  res += "}";
-  return res;
-}
- 
-template <size_t N>
-string to_string(bitset<N> v) {
-  string res = "";
-  for (size_t i = 0; i < N; i++) {
-    res += static_cast<char>('0' + v[i]);
-  }
-  return res;
-}
- 
-template <typename A>
-string to_string(A v) {
-  bool first = true;
-  string res = "{";
-  for (const auto &x : v) {
-    if (!first) {
-      res += ", ";
-    }
-    first = false;
-    res += to_string(x);
-  }
-  res += "}";
-  return res;
-}
- 
-template <typename A, typename B>
-string to_string(pair<A, B> p) {
-  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
-}
- 
-template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p) {
-  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
-}
- 
-template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p) {
-  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
-}
- 
-vector<long long> pow_counts(100);
- 
-void get_pow_counts(long long n, int p){
-  int cnt = 0;
- 
-  while(n > 0){
-    pow_counts[cnt] += n % p;
- 
-    cnt++;
-    n /= p;
-  }
+sura[i]=sura[i]/k;
  
 }
- 
-int main(){
- 
-  int t;
-  cin >> t;
- 
-  while(t--){
-    int n, k;
-    cin >> n >> k;
-    vector<long long> NADO(n);
- 
-    for(auto &x: NADO) cin >> x;
- 
-    pow_counts = vector<long long>(100);
- 
-    for(auto x : NADO) get_pow_counts(x, k);
- 
-    bool flag = true;
-    for(auto x : pow_counts){
-      if(x > 1){
-        flag = false;
+for(long long j=0;j<u.size();j++)
+{
+    if(u[j]==1 && ma[j])
+    {
+        hbe=false;
         break;
-      }
+ 
     }
-    if(flag){
-      cout << "YES\n";
-    }else{
-      cout << "NO\n";
+    else if(u[j]==1 && !ma[j])
+    {
+        ma[j]=true;
     }
-  }
  
  
  
-  return 0;
+}
+ 
+ 
+   }
+   if(hbe)
+    cout<<"YES"<<endl;
+   else
+    cout<<"NO"<<endl;
+ 
+ 
+ 
+}
+ 
+ 
 }
