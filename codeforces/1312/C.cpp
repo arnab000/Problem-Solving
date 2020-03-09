25932d6ca366a31c4745a05ef082b03da02384e9
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:56:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72853042

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,51 +1,69 @@
-#include<bits\stdc++.h>
-#define int long long
-using namespace std;
-main()
-{
-    int a,b,ok,i,j,s,tc;
-    cin>>tc;
-    while(tc--){
-            ok=1;
-    cin>>a>>b;
-    int aa[a];
-    for(i=0;i<a;i++)
-        cin>>aa[i];
-        map<int,int>mp;
-    for(i=0;i<a;i++)
-    {
-        if(aa[i]==0)
-            continue;
-        else
-        {
-            s=1,j=0;
-            while(aa[i]>0){
-            while(s<=aa[i])
-            {
-                j++;
-                s*=b;
-            }
-            s/=b;
-            if(j>0)
-            j--;
-          //  cout<<j<<endl;
-            if(mp[j]==1)
-            {
-                 //cout<<j<<endl;
-                ok=0;
-                break;
-            }
-            mp[j]=1;
-            aa[i]-=s;
-            j=0;
-            s=1;
-            }
+#include <bits/stdc++.h>
  
+using namespace std;
+ 
+#define Speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define mod 1000000007
+#define fr first
+#define sc second
+#define pb push_back
+#define PI acos(-1.0)
+#define print(num,ans) cout<<setprecision(num)<<fixed<<ans
+ 
+ll n,k;
+ll a[1000];
+ 
+vector<ll> con(ll x)
+{
+    vector<ll> v;
+    while(x > 0)
+    {
+        v.pb(x%k);
+        x /= k;
+    }
+    return v;
+}
+ 
+vector<ll> g;
+bool vis[1000];
+ 
+int main()
+{
+    Speed;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        g.clear();
+        memset(vis,0,sizeof vis);
+        cin>>n>>k;
+        bool b = 0;
+        for(int i=0;i<n;i++)cin>>a[i];
+ 
+        for(int i=0;i<n;i++)
+        {
+            if(!a[i])continue;
+            g = con(a[i]);
+            if(g.empty())continue;
+            for(int j=0;j<g.size();j++)
+            {
+                if(g[j] > 1 || (g[j] == 1 && vis[j]))
+                {
+                    b = 1;
+                    break;
+                }
+                else if(g[j] == 1)
+                {
+                    vis[j] = true;
+                }
+            }
+            if(b)break;
+            g.clear();
         }
+        if(b)cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+ 
     }
-    if(ok)
-        cout<<"YES"<<endl;
-    else
-        cout<<"NO"<<endl;
-    }
+    return 0;
 }
