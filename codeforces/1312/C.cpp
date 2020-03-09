9de6ea5f202d3bc3ecfc9a7cd25b655a1c9713c3
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 00:43:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72848433

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,69 +1,31 @@
-#include <bits/stdc++.h>
-#define sqr(a) ((a)*(a))
-#define rsz resize
-#define forr(i,a,b) for(int i=(a);i<(b);i++)
-#define forn(i,n) forr(i,0,n)
-#define dforn(i,n) for(int i=n-1;i>=0;i--)
-#define forall(it,v) for(auto it=v.begin();it!=v.end();it++)
-#define sz(c) ((int)c.size())
-#define zero(v) memset(v, 0, sizeof(v))
-#define pb push_back
-#define mp make_pair
-#define lb lower_bound
-#define ub upper_bound
-#define fst first
-#define snd second
- 
+#include<bits/stdc++.h>
 using namespace std;
- 
-typedef long long ll;
-typedef pair<int,int> ii;
-typedef vector<int> vi;
-typedef vector<ii> vii;
- 
 int main()
 {
-	#ifdef REJUNTE
-	freopen("input.in","r",stdin);
-	//freopen("output.out","w",stdout);
-	#endif
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
- 
-	ll k,n,aux,cont;
-	int t;
-	bool rta;
-	ll a;
-	bool used[60];
-	vector<ll>::iterator it;
-	cin >> t;
-	while(t--){
-		cin >> n >> k;
-		rta = true;
-		zero(used);
-		forn(i,n){
-			cin >> a;
-			while(a>0){
-				aux = 1;
-				cont = 0;
-				while(aux*k<=a){
-					cont++;
-					aux*=k;
-				}	
-				if(used[cont]){
-					rta = false;
-					break;
-				}
-				a-=aux;
-				used[cont]=true;
-			}
-		}
-		cout << (rta? "YES" : "NO") << endl;
- 
- 
-		
-	}
- 
-	return 0;
+    long long int i,j,k,n,m,s,c,t,a,con,x,p,vis[200];
+    vector<long long int >v;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>a;v.clear();
+        memset(vis,0,sizeof(vis));
+        con=0;
+        for(i=0;i<n;i++){cin>>k;v.push_back(k);}
+        for(i=0;i<n;i++)
+        {
+            x=v[i];
+            if(x==0) continue;
+            else
+               k=ceil(log(x)/log(a));
+            //cout<<"k"<<k<<endl;
+            for(j=k;j>=0;j--){  p=ceil(powl(a,j));
+                if(p<=x&&vis[j]==0) {
+                        x-=p;vis[j]=1;
+                }
+            }
+            if(x!=0){con=1;break;}
+        }
+        if(con==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
 }
