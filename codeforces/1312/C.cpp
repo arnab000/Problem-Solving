c4a67a2229b9e3299c0fd02910a71a1e0d67ffeb
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:35:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72851863

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,60 +1,67 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+ 
 using namespace std;
  
-long long int intlog(long long base, long long x) {
-    return (long long int)(log(x) / log(base));
-}
- 
- 
+int n,k,t;
+int fr[10001];
+long long sum;
  
 int main()
 {
-	int T;
-	cin>>T;
-	
-	while(T--)
-	{
-		int n,k;
-		cin>>n>>k;
-		int i;
-		vector<long long int> arr;
-		
-		for(i=0;i<n;i++)
-		{
-			long long int x;
-			cin>>x;
-			arr.push_back(x);
-		}
-		
-		int flag[100];
-		for(i=0;i<100;i++){
-			flag[i] = 0;
-		}
-		int flag1 = 0;
-		for(i=0;i<n;i++){
-			if(arr[i]!=0){
-				long long int x = log(arr[i])/log(k)+1;
-//				cout<<"x: "<<x<<"\n";
-				for(int j=x;j>=0;j--){
-					if(flag[j] == 0 && (long long int)pow(k,j)<=arr[i]){
-//						cout<<(long long int)pow(k,j)<<" ";
-						arr[i] = arr[i]-(long long int)pow(k,j);
-						flag[j] = 1;
-					}
-				}
-//				cout<<"\n";
-				if(arr[i] != 0){
-					flag1 = 1;
-					break;
-				}
-			}
-			
-		}
-		if(flag1==0){
-			cout<<"YES\n";
-		}
-		else{
-			cout<<"NO\n";
-		}
-	}
+    cin>>t;
+    while(t>0)
+    {
+        t--;
+        cin>>n>>k;
+ 
+        int ok=1;
+ 
+        for(int i=0; i<=100; i++)
+            fr[i]=0;
+ 
+        for(int i=1; i<=n; i++)
+        {
+            long long x;
+            cin>>x;
+            //cout<<x<<" ";
+            long long nr=1,p=0;
+ 
+            while(nr<x)
+            {
+                nr*=k;
+                p++;
+            }
+ 
+            if(nr>x)
+            {
+                nr/=k;
+                p--;
+            }
+ 
+            //cout<<nr<<endl;
+ 
+            while(p>=0 && x>0)
+            {
+                if(fr[p]==0 && x-nr>=0)
+                {
+                    fr[p]=1;
+                    x=x-nr;
+                }
+               // cout<<x<<endl;
+                nr/=k;
+                p--;
+            }
+ 
+            if(x!=0)
+                ok=0;
+        }
+ 
+        if(ok==0)
+            cout<<"NO"<<'\n';
+        else
+            cout<<"YES"<<'\n';
+ 
+    }
+    return 0;
 }
