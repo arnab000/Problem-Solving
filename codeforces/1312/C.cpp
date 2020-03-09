0fdd03892152d0132de1bb1a1ca70b29261d6133
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 00:35:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72847853

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,31 +1,63 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <set>
+#include <sstream>
+#define IOS                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define mod 1000000007
+#define eps 1e-6
+#define ll long long
+#define INF 0x3f3f3f3f
+#define MEM(x, y) memset(x, y, sizeof(x))
 using namespace std;
+int dt[][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {0, 0}};
+//typedef pair<int, int> P;
+//priority_queue<int, vector<int>, greater<int> > q;
+const int N = 1e4 + 10;
+ll a[N];
+bool f;
 int main()
 {
-    long long int i,j,k,n,m,s,c,t,a,con,x,p,vis[200];
-    vector<long long int >v;
-    cin>>t;
-    while(t--)
+    IOS;
+    int t;
+    cin >> t;
+    while (t--)
     {
-        cin>>n>>a;v.clear();
-        memset(vis,0,sizeof(vis));
-        con=0;
-        for(i=0;i<n;i++){cin>>k;v.push_back(k);}
-        for(i=0;i<n;i++)
+        ll n, k;
+        f = 1;
+        cin >> n >> k;
+        MEM(a, 0);
+        ll num;
+        ll cnt;
+        for (int i = 0; i < n; i++)
         {
-            x=v[i];
-            if(x==0) continue;
-            else
-               k=ceil(log(x)/log(a));
-            //cout<<"k"<<k<<endl;
-            for(j=k;j>=0;j--){  p=ceil(powl(a,j));
-                if(p<=x&&vis[j]==0) {
-                        x-=p;vis[j]=1;
+            cin >> num;
+            cnt = 0;
+            while (num)
+            {
+                a[cnt] += num % k;
+                num /= k;
+                if (a[cnt++] > 1)
+                {
+                    f = 0;
                 }
             }
-            if(x!=0){con=1;break;}
         }
-        if(con==0) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
+        if (f)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
 }
