80d2ce6eff0a3e08ddd644bef767192e31161360
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:39:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72852105

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,84 +1,61 @@
 #include<bits/stdc++.h>
+#define INF 10737418240000000
+#define ll long long
+#define PI (2*acos(0.0))
+#define Hare printf("here\n")
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define mp make_pair
+#define pb push_back
+#define pii pair<int,int>
+#define x first
+#define y second
+#define on(val,pos) val|(1<<pos)
+#define off(val,pos) val&(~(1<<pos))
+#define check(val,pos) (val&(1<<pos))
+ 
+//first four adjacent,second four corner
+//int row[8]={0,-1,0,1,1,-1,-1,1};
+//int col[8]={-1,0,1,0,-1,-1,1,1};
 using namespace std;
-typedef		long long       ll;
-typedef		vector<ll>      vll;
-#define		pb              push_back
-#define		PB              pop_back
-#define		nn              "\n"
-#define		all(p)          p.begin(),p.end()
-#define		zz(v)           (ll)v.size()
-#define		mms(ar,a)       memset(ar,a,sizeof(ar))
-#define		ss              ' '
-#define 	S(a)            scanf("%lld",&a)
-#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
-#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
-#define		gcd(a,b)        __gcd(a,b)
-#define		lcm(a,b)        (a*b)/gcd(a,b)
-#define		pi              acos(-1.0)
-#define		OO              100000000000000000
  
 int main(){
-//    freopen("input.txt","r",stdin);
-    ll a,b,c=0,i,j,t,k,lie,m,n,o,x,y,z,p=0;
-    S(t);
-    while(t--){
-        vll v,vv;
-        a=1;
-        ll ar[33],cnt=0;
-        ll tp[100]={0};
-        SS(n,k);
-        for(i=0;i<n;i++) S(ar[i]);
-        while(a<=OO){
-            v.pb(a);
-            a*=k;
-        }
-        for(i=0;i<n;i++){
-            if(ar[i]!=0){
-                bool bb=binary_search(all(v),ar[i]);
-                if(bb){
-                    auto it=lower_bound(all(v),ar[i]);
-                    z=it-v.begin();
-                    vv.pb(z);
-                }
-                else{
-                    vll vt;
-                    ll sm=0,p=0;
-                    x=ar[i];
-                    while(x){
-                        auto it=upper_bound(all(v),x);
-                        z=it-v.begin()-1;
-                        vt.pb(z);
-                        x-=v[z];
-                        if(z<0) break;
-                    }
-                    if(x==0){
-                        for(j=0;j<zz(vt);j++){
-                            vv.pb(vt[j]);
-                        }
-                    }
-                    else{
-                        cout<<"NO"<<nn;
-                        p=1;
-                        break;
-                    }
+    ll i,j,k,n,m,val,t=0,cnt,test;
+    //freopen("000input.txt","r",stdin);
+    cin>>test;
+    while(test--){
+        cin>>n>>k;
+        ll arr[n+5];
+ 
+        for(i=0;i<n;i++)scanf("%lld",&arr[i]);
+        sort(arr,arr+n);
+        ll ans=1LL;
+        vector<ll> po;
+        while(ans<=arr[n-1])po.pb(ans),ans*=k;
+        po.pb(INF);
+        map<ll,bool> done;
+ 
+        i=0;
+        while(arr[i]==0 and i<n)i++;
+ 
+        for(;i<n;i++){
+            while(arr[i]>0){
+                ll l=0,r=po.size()-1,mid;
+                while(l<=r){
+                    mid=(l+r)/2;
+                    if(po[mid]<=arr[i] and po[mid+1]>arr[i])break;
+                    else if(po[mid]>arr[i])r=mid-1;
+                    else l=mid+1;
                 }
  
+                if(l>r or done[po[mid]]==true)break;
+                else arr[i]-=po[mid],done[po[mid]]=true;
             }
  
+            if(arr[i]>0)break;
         }
-        if(p==1) continue;
-        sort(all(vv));
-        ll pp=0;
-        for(i=1;i<zz(vv);i++){
-            if(vv[i]==vv[i-1]){
-                cout<<"NO"<<nn;
-                pp=1;
-                break;
-            }
-        }
-        if(pp==1) continue;
-        cout<<"YES"<<nn;
+        if(i<n)printf("NO\n");
+        else printf("YES\n");
     }
- 
+    //free();   //if pointer array
     return 0;
 }
