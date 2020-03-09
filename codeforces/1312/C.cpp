3dacf3a4739b5f9f2dad3743ce486033a0efaa97
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:59:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72853203

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,83 +1,127 @@
-/*author:	Slnc*/
-	//freopen("input.txt","r",stdin);
-    //freopen("output.txt","w",stdout);
 #include <bits/stdc++.h>
-#include<string>
-#include <utility>/*useful*/
-#include <iomanip>//setprecision()
- 
-#define pb push_back
-#define pp pop_back
-#define ll long long
-#define pll pair<ll, ll>
-#define pii pair<int, int>
-#define vi vector<int>
-#define vs vector<string>
-#define vl vector<ll>
-#define mp make_pair
-#define all(v) v.begin(),v.end()
-#define ff first
-#define ss second
- 
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
  
-void NITRO(){
-	//MY CODE IS THE BEST :)
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-}
-const ll N = 1e+17;
-const int MOD = 1e+9 + 7;
-const ll INF = 1e+18;
+#define ll long long
+#define ldb long double
+#define db double
+#define str string
  
-void SOLVE() {
-    int n, k;
-    cin >> n >> k;
-    unsigned ll ma = 1;
-    set<ll> po;
-    while(ma <= N){
-        po.insert(ma);
-        ma *= k;
-    }
-    bool was[100], can = 1;
-    while(n--){
-        ll s;
-        cin >> s;
-        if(s != 0){
-            if(po.find(s) != po.end()){
-                po.erase(po.find(s));
-            }
-            else{
-                while(s > 0 && !po.empty()){
-                    if(po.find(s) != po.end()){
-                        po.erase(po.find(s));
-                        s = 0;
-                        break;
-                    }
-                    if(po.lower_bound(s) != po.end() && po.lower_bound(s) != po.begin()){
-                        set<ll>::iterator lb = po.lower_bound(s);
-                        --lb;
-                        s -= *lb;
-                        po.erase(*lb);
-                    }
-                    else break;
-                }
-                if(s != 0)can = 0;
-            }
-        }
-    }
-    if(can)cout << "YES";
-    else cout << "NO";
-    cout << "\n";
+#define fr first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define mp make_pair
+ 
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define pdb pair<db,db>
+#define pldb pair<ldb,ldb>
+ 
+#define vi vector<int>
+#define vll vector<ll>
+#define vdb vector<db>
+#define vldb vector<ldb>
+#define vpi vector< pi >
+#define vpll vector< pll >
+ 
+#define mi map<int,int>
+#define mll map<ll,ll>
+#define mdb map<db,db>
+#define mldb map<ldb,ldb>
+ 
+ 
+#define forr(i,a,b) for (int i = (a); i < (b); ++i)
+#define rrof(i,a,b) for (int i = (b)-1; i >= (a); --i)
+ 
+ 
+const int MOD = 1e9+7;
+const ldb PI = 4*atan((ldb)1);
+ 
+ll power(ll a,ll n){
+	ll res=1;
+ 
+	while(n){
+		if(n%2)
+		res*=a;
+		a*=a;
+		n/=2;
+	}
+	
+	return res;
 }
  
-int main() {
-    NITRO();
-    int t;
-    cin >> t;
-    while(t--) {
-        SOLVE();
-    }
-    return 0;
+int main(){
+	fast;
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+		int n,k;
+		cin>>n>>k;
+		
+		ll int a[n];
+		forr(i,0,n)
+		cin>>a[i];
+		
+		mll mpa;
+		int f=0;
+		
+		forr(i,0,n){
+			int cnt=0,e=1;
+			
+			while(e==1){
+				if(a[i]==0)
+				break;
+				
+				if(a[i]==1){
+					if(mpa[0]==1){
+						f=1;
+						cout<<"NO";
+						break;
+					}
+				}
+				
+				while(a[i]%k==0){
+					a[i]=a[i]/k;
+					cnt++;
+				}
+				
+				if(mpa[cnt]==0)
+				mpa[cnt]=1;
+				
+				else if(mpa[cnt]==1){
+					f=1;
+					cout<<"NO";
+					break;
+				}
+				
+				if(a[i]!=1){
+					a[i]--;
+					
+					if(a[i]%k!=0){
+						cout<<"NO";
+						f=1;
+						break;
+					}
+				}
+				
+				else if(a[i]==1)
+				break;
+			}
+			
+//			cout<<"Y";
+			if(f==1)
+			break;
+		}
+		
+		if(f==0)
+		cout<<"YES";
+		
+		cout<<"\n";
+	}
+	
+	return 0;
 }
