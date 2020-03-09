1b268a4112d63e9ce33a27fe1c81e3c65e2760a1
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:09:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72850216

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,41 +1,126 @@
-#include<bits/stdc++.h>
-using namespace std;
-long long a[40];
-long long ci[101];
-int t,n,k;
-int main()
-{
-	cin>>t;
-	while(t--)
-	{
-		memset(a,0,sizeof(a));
-		memset(ci,0,sizeof(ci));
-		cin>>n>>k;
-		int flag=0;
-		for(int i=1; i<=n; i++)
-		{
-			cin>>a[i];
-			int j=0;
-			while(a[i]>0)
-			{
-				ci[j]+=a[i]%k;
-				a[i]/=k;
-				j++;
-			}
-		}
-		for(int i=0; i<=100; i++)
-		{
-			if(ci[i]>1)
-				flag=1;
-		}
-		if(flag==1)
-		{
-			cout<<"NO"<<endl;
-			continue;
-		}
-		else
-			cout<<"YES"<<endl;
-	}
+#include <bits/stdc++.h>
  
-	return 0;
+using namespace std;
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+ 
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+ 
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+ 
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+ 
+vector<long long> pow_counts(100);
+ 
+void get_pow_counts(long long n, int p){
+  int cnt = 0;
+ 
+  while(n > 0){
+    pow_counts[cnt] += n % p;
+ 
+    cnt++;
+    n /= p;
+  }
+ 
+}
+ 
+int main(){
+ 
+  int t;
+  cin >> t;
+ 
+  while(t--){
+    int n, k;
+    cin >> n >> k;
+    vector<long long> NADO(n);
+ 
+    for(auto &x: NADO) cin >> x;
+ 
+    pow_counts = vector<long long>(100);
+ 
+    for(auto x : NADO) get_pow_counts(x, k);
+ 
+    bool flag = true;
+    for(auto x : pow_counts){
+      if(x > 1){
+        flag = false;
+        break;
+      }
+    }
+    if(flag){
+      cout << "YES\n";
+    }else{
+      cout << "NO\n";
+    }
+  }
+ 
+ 
+ 
+  return 0;
 }
