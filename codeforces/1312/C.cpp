0fa8f8677f1ff31dfab5755a6f1aa112080ecfd4
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 02:46:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72855309

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,82 +1,70 @@
-#pragma GCC optimizte("Ofast,fast-math,unroll-loops")
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <vector>
-#include <string>
-#include <stdio.h>
-#include <algorithm>
-#include <set>
-#include <map>
-#include <math.h>
-#include <cmath>
-#include <queue>
-#include <iomanip>
-#include <bitset>
-#include <unordered_map>
-#include <stack>
-#include <memory.h>
-#include <list>
-#include <numeric>
-#include <functional>
-#include <complex>
-#include <cassert>
-#include <random>
- 
-#pragma comment(linker, "/STACK:36777216")
- 
+#include <bits/stdc++.h>
+#define ll long long 
 using namespace std;
- 
-typedef unsigned long long ull;
-typedef long long ll;
-typedef long double ld;
- 
-int main() {
-#ifdef _DEBUG
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#else
-	//freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
-	//freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
-#endif
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	cout.precision(20); cout.setf(ios::fixed);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 	int t;
-	cin >> t;
- 
-	while (t--) {
+	cin>>t;
+	while(t--){
 		ll n, k;
-		cin >> n >> k;
-		set<ull>used;
-		vector<ull>pows;
-		ll v = 1;
-		for (int j = 0; ; j++) {
-			pows.emplace_back(v);
-			if (pows.back() >= 1e16) break;
-			v *= k;
+		cin>>n>>k;
+		vector<ll> arr;
+		for(int i = 0; i < n; i++){
+			ll a;
+			cin>>a;
+			arr.push_back(a);
 		}
- 
-		int ok = 1;
-		for (int i = 0; i < n; i++) {
-			ll x;
-			cin >> x;
-			for (int j = (int)pows.size() - 1; j >= 0; j--) {
-				if (x >= pows[j]) {
-					x -= pows[j];
-					if (!used.insert(j).second) {
-						ok = 0;
+		vector<ll> pre;
+		ll at = 1;
+		pre.push_back(at);
+		for(int i = 1; i <= 64;i++){
+			at *= k;
+			if(at < 0)
+				break;
+			pre.push_back(at);
+		}
+		sort(pre.begin(),pre.end());
+		sort(arr.begin(),arr.end());
+		bool okay = true;
+		bool used[1000];
+		memset(used,false,sizeof(used));
+		for(int i = 0; i < n; i++){
+				bool ok = true;
+				while(arr[i] > 0){
+					ll pos = upper_bound(pre.begin(), pre.end(), arr[i]) - pre.begin();
+					ll value = pre[pos];
+					value /= k;
+					pos--;
+					//cout<<arr[i]<<" ";
+					if(pos < 0)
+					{
+						pos = 0;
+						value = 1;
+					}
+					if(used[pos])
+					{
+						ok = false;
 						break;
 					}
+					else
+					{
+						arr[i] -= pre[pos];
+						used[pos] = true;
+					}
 				}
-			}
- 
-			if (x) ok = 0;
+				if(!ok)
+				{
+					okay = false;
+					break;
+				}
 		}
- 
-		if (ok) puts("YES");
-		else puts("NO");
+		
+		if(okay)
+			puts("YES");
+		else
+			puts("NO");
 	}
 	return 0;
 }
