47ef5853bde5af987c1b463adc301bc68dd24139
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 00:24:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72847088

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,63 +1,74 @@
-#include <iostream>
-#include <cstdio>
-#include <fstream>
-#include <algorithm>
-#include <cmath>
-#include <iomanip>
-#include <deque>
-#include <vector>
-#include <queue>
-#include <string>
-#include <cstring>
-#include <map>
-#include <stack>
-#include <set>
-#include <sstream>
-#define IOS                       \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);
-#define mod 1000000007
-#define eps 1e-6
-#define ll long long
-#define INF 0x3f3f3f3f
-#define MEM(x, y) memset(x, y, sizeof(x))
+#include<bits/stdc++.h>
 using namespace std;
-int dt[][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {0, 0}};
-//typedef pair<int, int> P;
-//priority_queue<int, vector<int>, greater<int> > q;
-const int N = 1e4 + 10;
-ll a[N];
-bool f;
+
 int main()
 {
-    IOS;
-    int t;
-    cin >> t;
-    while (t--)
+long long test;
+cin>>test;
+while(test--)
+{
+   long long n,k;
+   cin>>n>>k;
+   vector<long long>sura;
+   bool hbe=true;
+   for(long i=0;i<n;i++)
+   {
+       long long l;
+       cin>>l;
+       sura.push_back(l);
+
+   }
+
+   if(k==1)
+   {
+           cout<<"YES"<<endl;
+           return 0;
+
+   }
+   map<long long ,bool>ma;
+    for(long i=0;i<n;i++)
+   {
+       vector<long long>u;
+       if(sura[i]==0)
+        continue;
+   for(long long j=0; sura[i]>0; j++)
+{
+u.push_back(sura[i]%k);
+if(sura[i]%k>1)
+{
+    hbe=false;
+    break;
+}
+sura[i]=sura[i]/k;
+
+}
+for(long long j=0;j<u.size();j++)
+{
+    if(u[j]==1 && ma[j])
     {
-        ll n, k;
-        f = 1;
-        cin >> n >> k;
-        MEM(a, 0);
-        ll num;
-        ll cnt;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> num;
-            cnt = 0;
-            while (num)
-            {
-                a[cnt] += num % k;
-                num /= k;
-                if (a[cnt++] > 1)
-                {
-                    f = 0;
-                }
-            }
-        }
-        if (f)
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        hbe=false;
+        break;
+
     }
+    else if(u[j]==1 && !ma[j])
+    {
+        ma[j]=true;
+    }
+
+
+
+}
+
+
+   }
+   if(hbe)
+    cout<<"YES"<<endl;
+   else
+    cout<<"NO"<<endl;
+
+
+
+}
+
+
 }
