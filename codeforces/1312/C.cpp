5b8e22a8820d4fee73ba8b9e585e9121240561ee
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-10 01:55:00 +0600

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72852979

--- codeforces/1312/C.cpp
+++ codeforces/1312/C.cpp
@@ -1,69 +1,58 @@
-#include <bits/stdc++.h>
- 
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<stdio.h>
+#include<vector>
+#include<cstring>
 using namespace std;
- 
-#define Speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define ll long long
-#define mod 1000000007
-#define fr first
-#define sc second
-#define pb push_back
-#define PI acos(-1.0)
-#define print(num,ans) cout<<setprecision(num)<<fixed<<ans
- 
-ll n,k;
-ll a[1000];
- 
-vector<ll> con(ll x)
-{
-    vector<ll> v;
-    while(x > 0)
-    {
-        v.pb(x%k);
-        x /= k;
-    }
-    return v;
-}
- 
-vector<ll> g;
-bool vis[1000];
- 
+long long a[200000];
+long long ans;
 int main()
 {
-    Speed;
-    int t;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    long long t;
     cin>>t;
     while(t--)
     {
-        g.clear();
-        memset(vis,0,sizeof vis);
-        cin>>n>>k;
-        bool b = 0;
-        for(int i=0;i<n;i++)cin>>a[i];
- 
-        for(int i=0;i<n;i++)
+        
+        memset(a,0,sizeof(a));
+        long long vis=1;
+       long long n,k;
+       long long num;
+       cin>>n>>k;
+       for(long long i=0;i<n;i++)
         {
-            if(!a[i])continue;
-            g = con(a[i]);
-            if(g.empty())continue;
-            for(int j=0;j<g.size();j++)
+            long long len=0;
+            cin>>num;
+            while(num)
             {
-                if(g[j] > 1 || (g[j] == 1 && vis[j]))
+                a[len]+=num%k;
+                num/=k;
+                if(a[len++]>1)
                 {
-                    b = 1;
-                    break;
-                }
-                else if(g[j] == 1)
-                {
-                    vis[j] = true;
+                    vis=0;
                 }
             }
-            if(b)break;
-            g.clear();
+ 
         }
-        if(b)cout<<"NO"<<endl;
-        else cout<<"YES"<<endl;
+        if(vis==0)
+            
+            {
+                cout<<"NO"<<endl;
+            }
+            else
+            {
+                cout<<"YES"<<endl;
+            }
+ 
  
     }
+ 
+ 
+ 
+    
+ 
     return 0;
 }
