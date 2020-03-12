7810ecb22ce9bc95104c7c5a920e7259255a2441
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-12 22:42:00 +0600

    Add solution for problem `B - Yet Another Palindrome Problem` Link: https://codeforces.com/contest/1324/problem/B Tags: brute force, strings, *1100 Ref: https://codeforces.com/contest/1324/submission/73089539

--- codeforces/1324/B.cpp
+++ codeforces/1324/B.cpp
@@ -1,35 +1,42 @@
-#include <bits/stdc++.h> 
- 
+#include<bits/stdc++.h>
 using namespace std;
-using ll=long long;
-using ull=unsigned long long;
-using pii=pair<int,int>;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n+10];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
  
-#define INF LONG_MAX
-#define MOD 1000000007
-#define rng(a) a.begin(),a.end()
-#define rrng(a) a.end(),a.begin()
-#define endl "\n"
- 
-int main(){
-  ios::sync_with_stdio(false);
-  cin.tie(0);
- 
-  int t;
-  cin>>t;
-  while(t--){
-    int n;
-    cin>>n;
-    vector<int>a(n);
-    for(int i=0;i<n;i++)cin>>a[i];
-    bool ok=false;
-    for(int i=0;i<n;i++){
-      for(int j=i+2;j<n;j++){
-        if(a[i]==a[j])ok=true;
-      }
+        }
+        int c=0;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(a[i]==a[j]&&abs(j-i)>1)
+                {
+                    c=1;
+                    break;
+                }
+            }
+            if(c==1)
+            {
+                break;
+            }
+        }
+        if(c==1)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
     }
-    if(ok)cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;
-  }
-  return 0;
 }
