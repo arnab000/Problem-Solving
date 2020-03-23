34b9d67ffd98db6b34643a8297f50696d35d81be
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-23 22:19:00 +0600

    Add solution for problem `A - Sum of Odd Integers` Link: https://codeforces.com/contest/1327/problem/A Tags: math, *1100 Ref: https://codeforces.com/contest/1327/submission/74116320

--- codeforces/1327/A.cpp
+++ codeforces/1327/A.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
     using namespace std;
- 
- 
+
+
     int main()
     {
       
@@ -23,6 +23,6 @@
             }else{
                 cout<<"NO"<<endl;
             }
- 
+
         }
     }
