e06a1554f9c19da841fe0db5f19ee6113d8de2b3
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-03-24 09:06:00 +0600

    Add solution for problem `A - Sum of Odd Integers` Link: https://codeforces.com/contest/1327/problem/A Tags: math, *1100 Ref: https://codeforces.com/contest/1327/submission/74157875

---
+++ codeforces/1327/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    long long test;
+    cin>>test;
+    while(test--)
+    {
+       long long n,k;
+       cin>>n>>k;
+   
+       if(n>=k*k && (n-(k*k))%2==0)
+       {
+           cout<<"YES"<<endl;
+       }
+       else
+        cout<<"NO"<<endl;
+
+
+
+    }
+}
