3b946425d8a115c19ab8138905cd8f6532697de5
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-04-01 05:35:00 +0600

    Add solution for problem `D - Walk on Matrix` Link: https://codeforces.com/contest/1332/problem/D Tags: bitmasks, constructive algorithms, math, *1700 Ref: https://codeforces.com/contest/1332/submission/75026814

---
+++ codeforces/1332/D.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+long long n;
+cin>>n;
+long long h=1<<17;
+cout<<2<<" "<<3<<endl;
+
+
+cout<<h+n<< " "<<n<<" "<<0<<endl;
+
+cout<<h<< " "<<h+n<<" "<<n<<endl;
+}
