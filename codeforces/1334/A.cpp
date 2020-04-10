ff83f84d9ad22bf53fc484b9149f30ec1142c381
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-04-10 22:54:00 +0600

    Add solution for problem `A - Level Statistics` Link: https://codeforces.com/contest/1334/problem/A Tags: implementation, math, *1200 Ref: https://codeforces.com/contest/1334/submission/76191451

--- codeforces/1334/A.cpp
+++ codeforces/1334/A.cpp
@@ -1,37 +1,27 @@
+
 #include<bits/stdc++.h>
 using namespace std;
-int t;
-int p[203],c[203];
-		int n;
-int main()
-{
-	scanf("%d",&t);
-	p[0]=0;
-	c[0]=0;
-	while(t--)
-	{
-		scanf("%d",&n);
-		for(int i=1;i<=n;i++)
-		{
-			scanf("%d%d",&p[i],&c[i]);
-		}
-		int flag=0;
-		int mp=0,mc=0;
-		for(int i=1;i<=n;i++)
-		{
-			if(p[i]>=mp&&c[i]>=mc&&p[i]>=c[i]&&mp>=mc&&p[i]-p[i-1]>=c[i]-c[i-1])
-			{
-				mp=max(mp,p[i]);
-				mc=max(mc,c[i]);
-				continue;
-			}
-			else 
-			{
-				flag=1;
-				break;
-			}
-		}
-		if(flag) puts("NO");
-		else puts("YES");
-	}
+#define ll long long
+ 
+void solve(ll cs) {
+    ll n, pi, ci, lpi = INT_MIN, lci = INT_MIN;
+    cin >> n;
+    bool yes = true;
+    for (ll i = 0; i < n; i++) {
+        cin >> pi >> ci;
+        if(pi < ci || pi < lpi || ci < lci || (i > 0 && (pi - lpi) < (ci - lci))){
+            yes = false;
+        }
+        lpi = pi; lci = ci;
+    }
+    cout << (yes ? "YES" : "NO") << endl;
+}
+ 
+int main() {
+    ll t;
+    cin >> t;
+    for(ll i = 0; i < t; i++) {
+        solve(i+1);
+    }
+    return 0;
 }
