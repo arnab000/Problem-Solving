0cddf1c1910b55b0a85a0c8656259add26a98ad5
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-04-11 03:04:00 +0600

    Add solution for problem `C - Circle of Monsters` Link: https://codeforces.com/contest/1334/problem/C Tags: brute force, constructive algorithms, greedy, math, *1600 Ref: https://codeforces.com/contest/1334/submission/76214678

---
+++ codeforces/1334/C.cpp
@@ -0,0 +1,53 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);cin.tie(0);
+    long long test;
+    cin>>test;
+    while(test--)
+    {
+        long long n;
+        cin>>n;
+        long long sura[n+1],sum[n+1],mura[n+1];
+        long long mn=1e18;
+       long long mn1=INT_MAX;
+        for(long long i=1;i<=n;i++)
+        {
+            cin>>sura[i];
+            cin>>mura[i];
+         mn=min(mn,sura[i]);
+        }
+
+    sum[0]=sura[1]-mura[n];
+          for(long long i=1;i<n;i++)
+        {
+           sum[i]=(sura[i+1]-mura[i]);
+        }
+        long long ans=0;
+        long long mx=-1,haha;
+
+    bool pasa=false;
+          for(long long i=1;i<=n;i++)
+        {
+         if(sum[i-1]>0)
+         {
+           mn=min(mn,sura[i]-sum[i-1]);
+           //pasa=true;
+         }
+
+        }
+
+
+        for(long long i=0;i<n;i++)
+        {
+            if(sum[i]>0)
+                ans+=sum[i];
+
+        }
+        cout<<ans+mn<<endl;
+    }
+}
+
