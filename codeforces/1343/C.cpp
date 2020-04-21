09f9319531cb0d71ab6c2749feb03949b8ea4f7f
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-04-22 03:16:00 +0600

    Add solution for problem `C - Alternating Subsequence` Link: https://codeforces.com/contest/1343/problem/C Tags: dp, greedy, two pointers, *1200 Ref: https://codeforces.com/contest/1343/submission/77591455

--- codeforces/1343/C.cpp
+++ codeforces/1343/C.cpp
@@ -1,71 +1,56 @@
-    #include <bits/stdc++.h>
-    using namespace std;
-     
-     
-     
-     
-     
-    int main() {
-        long long test;
-        cin>>test;
-     
-     
-     
-        while(test--) {
-       long long n;
-       cin>>n;
-       vector<long long>ans;
-     
-       for(long long i=0;i<n;i++)
-       {
-       long long k;
-       cin>>k;
-       ans.push_back(k);
-     
-       }
-       long long d=0;
-       long long mx=0,mn=0;
-       bool p=false,m=false;
-          for(long long i=0;i<n;i++)
-       {
-       if(ans[i]<0)
-       {
-       if(!m)
-       {
-       d+=mx;
-       m=true;
-       p=false;
-       mn=ans[i];
-       }
-       else
-       mn=max(mn,ans[i]);
-     
-       }
-      else  if(ans[i]>0)
-       {
-       if(!p)
-       {
-       d+=mn;
-       p=true;
-       m=false;
-       mx=ans[i];
-       }
-       else
-       mx=max(mx,ans[i]);
-     
-       }
-     
-       }
-       if(p)
-       d+=mx;
-       else
-       d+=mn;
-       cout<<d<<endl;
-     
-     
-     
-     
-     
-        }
-     
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+#define pb push_back
+#define nl '\n'
+#define deb(x) cerr<<#x" = "<<x<<nl
+#define in() ( { int a ; scanf("%d",&a); a; } )
+ 
+ 
+const int N = 5e5 + 9;
+const int mod = 1e9 + 7;
+ll n,x,t,ans=INT_MIN,dp[2*(1009)][2*100];
+vector<ll>v ;
+ 
+ 
+int main()
+{
+  // freopen("input.txt", "r+", stdin);
+  // freopen("output.txt", "w+", stdout);
+ 
+     ios_base::sync_with_stdio(0);cin.tie(nullptr); cout.tie(nullptr); 
+    
+    cin>>t;
+    while(t--){
+ 
+          cin>>n;
+          v.clear();
+          ans=0;
+          ll mn=INT_MIN,mx=INT_MAX;
+          for(int i=0; i<n; i++){
+ 
+            cin>>x;
+            if(!i){v.pb(x) ; mn=x; mx=x; continue;  }
+            if(signbit(x)!=signbit(v.back() )){
+                  
+                 //// cout<<" COL "<<i<<endl;
+                  if(signbit(v.back())==-1) ans+=mn;
+                  else ans+=mx;
+                  mn=x; mx=x;
+                   v.clear();
+            }
+            v.pb(x);
+            mn=min(mn,x);
+            mx=max(mx,x);
+          }
+          if(signbit(v.back())==-1) ans+=mn;
+           else ans+=mx;
+          cout<<ans<<endl;
+          
     }
+ 
+  return 0;
+}
+ 
+ 
