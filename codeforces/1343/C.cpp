1b26022c93a9ec764594e3dc792bf1fad343d563
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-04-22 03:17:00 +0600

    Add solution for problem `C - Alternating Subsequence` Link: https://codeforces.com/contest/1343/problem/C Tags: dp, greedy, two pointers, *1200 Ref: https://codeforces.com/contest/1343/submission/77591480

--- codeforces/1343/C.cpp
+++ codeforces/1343/C.cpp
@@ -1,62 +1,71 @@
-    #include<bits/stdc++.h>
+    #include <bits/stdc++.h>
     using namespace std;
-    int main()
-    {
-        long long t;
-        cin >> t;
-        while(t--)
-        {
-            long long n,i,j,x,mx,s1=0,s2=0,l1,l2;
-            cin >> n;
-            vector<long long> v(n);
-            for(i=0; i<n; i++)
-                cin >> v[i];
-            vector<long long> tmp1,tmp2;
-            for(i=0; i<n; i++){
-              if(tmp1.empty() || tmp1.back()<0){
-                if(v[i]<0) continue;
-                mx=0;
-                while(i<n && v[i]>0) mx=max(mx,v[i]),i++;
-                i--;
-                if(mx!=0){
-                  tmp1.push_back(mx);
-                }
-                continue;
-              }
-              if(v[i]>0) continue;
-              mx=   INT_MIN;
-              while(i<n && v[i]<0) mx=max(mx,v[i]),i++;
-              i--;
-              if(mx!=INT_MIN){
-                tmp1.push_back(mx);
-              }
-            }
-            for(i=0; i<n; i++){
-              if(tmp2.empty() || tmp2.back()>0){
-                if(v[i]>0) continue;
-                mx=INT_MIN;
-                while(i<n && v[i]<0) mx=max(mx,v[i]),i++;
-                i--;
-                if(mx!=INT_MIN){
-                  tmp2.push_back(mx);
-                }
-                continue;
-              }
-              if(v[i]<0) continue;
-              mx=0;
-              while(i<n && v[i]>0) mx=max(mx,v[i]),i++;
-              i--;
-              if(mx!=0){
-                tmp2.push_back(mx);
-              }
-            }
-            l1=tmp1.size();
-            l2=tmp2.size();
-            for(i=0; i<l1; i++) s1+=tmp1[i];
-            for(i=0; i<l2; i++) s2+=tmp2[i];
-            if(l1>l2) cout << s1 << "\n";
-            else if(l2>l1) cout << s2 << "\n";
-            else cout << max(s1,s2) << "\n";
+     
+     
+     
+     
+     
+    int main() {
+        long long test;
+        cin>>test;
+     
+     
+     
+        while(test--) {
+       long long n;
+       cin>>n;
+       vector<long long>ans;
+     
+       for(long long i=0;i<n;i++)
+       {
+       long long k;
+       cin>>k;
+       ans.push_back(k);
+     
+       }
+       long long d=0;
+       long long mx=0,mn=0;
+       bool p=false,m=false;
+          for(long long i=0;i<n;i++)
+       {
+       if(ans[i]<0)
+       {
+       if(!m)
+       {
+       d+=mx;
+       m=true;
+       p=false;
+       mn=ans[i];
+       }
+       else
+       mn=max(mn,ans[i]);
+     
+       }
+      else  if(ans[i]>0)
+       {
+       if(!p)
+       {
+       d+=mn;
+       p=true;
+       m=false;
+       mx=ans[i];
+       }
+       else
+       mx=max(mx,ans[i]);
+     
+       }
+     
+       }
+       if(p)
+       d+=mx;
+       else
+       d+=mn;
+       cout<<d<<endl;
+     
+     
+     
+     
+     
         }
-        return 0;
+     
     }
