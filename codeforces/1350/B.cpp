a981d96b005988e6e360a1b2be8e8df5d79acac9
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-05-13 09:47:00 +0600

    Add solution for problem `B - Orac and Models` Link: https://codeforces.com/contest/1350/problem/B Tags: dp, math, number theory, *1400 Ref: https://codeforces.com/contest/1350/submission/79938675

--- codeforces/1350/B.cpp
+++ codeforces/1350/B.cpp
@@ -1,70 +1,52 @@
 #include<bits/stdc++.h>
+    
 using namespace std;
-#define dofast ios::sync_with_stdio(false);cin.tie(0);
 #define ll long long
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define ld long double
-#define MAX 100000
-bool comparator(pair<ll,ll> a,pair<ll,ll> b){
-}
- 
-vector<int>divs;
-set<int>pr[100005];
-bool vis[MAX+1];
-void get_all(){
-    for(int i=1;i<=100001;i++)
-    {
-        for(int j=1; j*j<=i;j++)
-        {
-            if(i%j==0)
-            {
-                int num=(i/j);
-                if(num!=j)
-                pr[i].insert(num);
-                pr[i].insert(j);
-            }
-        }
-    }
- 
- 
- 
-}
- 
- 
- 
+#define f first
+#define s second
+#define Fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+     
 int main()
 {
-    dofast
-    get_all();
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        //vector<ll>vec(n);
-        ll arr[n+2];
-        int chk[n+2];
-        for(int i=1;i<=n;i++){
-            cin>>arr[i];
-            chk[i]=1;
-        }
-        int ans=1;
-        for(int i=2;i<=n;i++){
-            int tempans=1;
-            for(auto it=pr[i].begin();it!=pr[i].end();it++){
-                if(arr[*it]<arr[i]){
-                    tempans=max(tempans,chk[*it]+1);
-                    //cout<<tempans<<endl;
+      ll test;
+      cin>>test;
+      while(test--)
+      {
+          ll n;
+         
+          cin>>n;
+          ll sura[n+2];
+          for (ll i = 1; i <= n; i++)
+          {
+             cin>>sura[i];
+              
+
+          }
+          ll arr[n+1];
+          for(ll i=1;i<=n;i++)
+          {
+              arr[i]=1;
+          }
+          ll ans =1;
+          for (ll i = n; i >= 1; i--)
+          {
+             for(ll j=2;i*j<=n;j++)
+             {
+                if(sura[i*j]>sura[i])
+                {
+                    arr[i]=max(arr[i],1+arr[i*j]);
                 }
-            }
-            chk[i]=tempans;
-            ans=max(ans,tempans);
- 
-        }
-        cout<<ans<<endl;
-    }
-    return 0;
+
+             }
+             ans=max(ans,arr[i]);
+
+          }
+          cout<<ans<<endl;
+          
+
+      }
+      
+    
+        
+
 }
