63cedb975d6a8ca97180a994f8ccee2266f72760
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-07-01 19:41:00 +0600

    Add solution for problem `A - Magical Sticks` Link: https://codeforces.com/contest/1371/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1371/submission/85633610

---
+++ codeforces/1371/A.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+    
+using namespace std;
+#define ll long long
+#define f first
+#define s second
+#define Fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fo(a,b,c) for(int i=a;i<b;i+=c)  
+typedef pair<ll , pair<ll, ll> > pi;
+int pow(int x,int y){
+    int res=1;
+    while(y){
+        if(y&1) res*=x;
+        y>>=1;
+        x*=x;
+    }
+    return res;
+}   
+struct Compare {
+    constexpr bool operator()(pi const & a,
+                              pi const & b) const noexcept
+    { return a.first < b.first || (a.first == b.first && a.second.first > b.second.first); }
+ 
+};
+void prefix_function( string s,ll arr[] )
+{
+    
+   
+    long long border=0;
+    arr[0]=0;
+    for(long long i=1;i<s.size();i++)
+    {
+      while(border>0 && s[i]!=s[border])
+      {
+          border=arr[border-1];
+      }
+      if(s[i]==s[border])
+      {
+          border++;
+      }
+      else
+      {
+          border=0;
+      }
+      arr[i]=border;
+      
+    }
+    
+
+}
+
+
+
+int main()
+{
+ ll test;
+ cin>>test;
+ while(test--)
+ {
+     ll n;
+     cin>>n;
+     if(n%2==0)
+     cout<<n/2<<endl;
+     else
+     {
+         cout<<n/2+1<<endl;
+     }
+     
+
+ }
+
+
+}
+
+
+
+   
+      
+    
+        
+
