118f17a1a194fb5e18b080ac33167d8f86f8a01c
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-07-19 18:01:00 +0600

    Add solution for problem `B - Dubious Cyrpto` Link: https://codeforces.com/contest/1379/problem/B Tags: binary search, brute force, math, number theory, *1500 Ref: https://codeforces.com/contest/1379/submission/87347988

---
+++ codeforces/1379/B.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+    
+using namespace std;
+#define ll long long
+#define f first
+#define s second
+#define Fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fo(a,b,c) for(int i=a;i<b;i+=c)  
+typedef pair<ll , pair<ll, ll> > pi;
+int pow(int x,int y){
+    int res=1;
+    while(y){
+        if(y&1) res*=x;
+        y>>=1;
+        x*=x;
+    }
+    return res;
+}   
+struct Compare {
+    constexpr bool operator()(pi const & a,
+                              pi const & b) const noexcept
+    { return a.first < b.first || (a.first == b.first && a.second.first > b.second.first); }
+ 
+};
+void prefix_function( string s,ll arr[] )
+{
+    
+   
+    long long border=0;
+    arr[0]=0;
+    for(long long i=1;i<s.size();i++)
+    {
+      while(border>0 && s[i]!=s[border])
+      {
+          border=arr[border-1];
+      }
+      if(s[i]==s[border])
+      {
+          border++;
+      }
+      else
+      {
+          border=0;
+      }
+      arr[i]=border;
+      
+    }
+    
+
+}
+
+
+
+int main()
+{
+    Fast
+ ll test;
+ cin>>test;
+ while(test--)
+ {
+     ll l,r,m;
+     cin>>l>>r>>m;
+     ll dif=r-l;
+     for(ll i=l;i<=r;i++)
+     {
+         bool hbe=false;
+         ll x=m%i;
+         ll y=i-(m%i);
+         if(m%i<=dif && m>=i)
+         {
+            
+                 cout<<i<<" "<<l+(m%i)<<" "<<l<<endl;
+            
+             
+             break;
+         }
+         if(y<=dif)
+         {
+         
+                 cout<<i<<" "<<l<<" "<<l+y<<endl;
+                 hbe=true;
+           
+            
+             
+             
+         }
+         if(hbe)
+         break;
+
+     }
+
+ }
+
+
+}
+
+
+
+   
+      
+    
+        
+
