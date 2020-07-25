fd8a4599df7fdbcc70156292db3fcb242f65916f
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-07-25 13:34:00 +0600

    Add solution for problem `C - Stack of Presents` Link: https://codeforces.com/contest/1279/problem/C Tags: data structures, implementation, *1400 Ref: https://codeforces.com/contest/1279/submission/87967577

---
+++ codeforces/1279/C.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+    
+using namespace std;
+#define ll long long
+#define f first
+#define s second
+#define Fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fo(a,b,c) for(int i=a;i<b;i+=c)  
+typedef pair<ll , pair<ll, ll> > pi;
+int pow(int x,int y){
+    int res=1;
+    while(y){
+        if(y&1) res*=x;
+        y>>=1;
+        x*=x;
+    }
+    return res;
+}   
+struct Compare {
+    constexpr bool operator()(pi const & a,
+                              pi const & b) const noexcept
+    { return a.first < b.first || (a.first == b.first && a.second.first > b.second.first); }
+ 
+};
+void prefix_function( string s,ll arr[] )
+{
+    
+   
+    long long border=0;
+    arr[0]=0;
+    for(long long i=1;i<s.size();i++)
+    {
+      while(border>0 && s[i]!=s[border])
+      {
+          border=arr[border-1];
+      }
+      if(s[i]==s[border])
+      {
+          border++;
+      }
+      else
+      {
+          border=0;
+      }
+      arr[i]=border;
+      
+    }
+    
+
+}
+
+
+
+int main()
+{
+ ll test;
+ cin>>test;
+ while(test--)
+ {
+     ll n,m;
+     cin>>n>>m;
+     map<ll,ll>sura;
+     vector<ll>ma;
+     for(ll i=0;i<n;i++)
+     {
+        ll k;
+        cin>>k;
+        sura[k]=i;
+     }
+     ll mx=-1;
+     ll ans=0;
+     for(ll i=0;i<m;i++)
+     {
+         ll k;
+         cin>>k;
+         if(mx<sura[k])
+         {
+             ans+=(2*(sura[k]-i))+1;
+         }
+         else
+         {
+             ans+=1;
+         }
+         mx=max(mx,sura[k]);
+         
+     }
+     cout<<ans<<endl;
+
+ }
+
+
+}
+
+
+
+   
+      
+    
+        
+
