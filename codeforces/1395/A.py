9d121688939b1f68f39ad9135d4fc6a1b1889777
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-08-13 03:16:00 +0600

    Add solution for problem `A - Boboniu Likes to Color Balls` Link: https://codeforces.com/contest/1395/problem/A Tags: brute force, math, *1000 Ref: https://codeforces.com/contest/1395/submission/89744944

--- codeforces/1395/A.py
+++ codeforces/1395/A.py
@@ -1,3 +1,4 @@
+
 x = int(input())
 for i in range(1, x + 1, 1):
     count = 0
@@ -13,21 +14,23 @@ for i in range(1, x + 1, 1):
 
     for j in range(0, 3, 1):
         mn = min(mn, sura[j])
-    if mn < 1:
+    if mn<1:
         print("No")
         continue
 
+
     for j in range(0, 3, 1):
         sura[j] -= 1
-    sura[3] += 3 
-    count = 0
+    sura[3] += 3 * 1
+    count=0
     for j in range(0, 4, 1):
         if sura[j] % 2 == 0:
             count += 1
-
+ 
     if count >= 3:
         print("Yes")
     else:
         print("No")
 
 
+        
