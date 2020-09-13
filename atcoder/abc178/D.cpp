5139b078e3826b9f5c085b4e15c0d37aa0cb8403
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-09-13 19:11:00 +0600

    Add solution for problem `D - Redistribution` Link: https://atcoder.jp/contests/abc178/tasks/abc178_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc178/submissions/16710889

---
+++ atcoder/abc178/D.cpp
@@ -0,0 +1,183 @@
+/*#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define f first
+#define s second
+#define Fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+typedef pair<ll , pair<ll, ll> > pi;
+int pow(int x,int y){
+    int res=1;
+    while(y){
+        if(y&1) res*=x;
+        y>>=1;
+        x*=x;
+    }
+    return res;
+}   
+struct Compare {
+    constexpr bool operator()(pi const & a,
+                              pi const & b) const noexcept
+    { return a.first < b.first || (a.first == b.first && a.second.first > b.second.first); }
+ 
+};
+void prefix_function( string s,ll arr[] )
+{
+     long long border=0; arr[0]=0;
+    for(long long i=1;i<s.size();i++)
+    {
+      while(border>0 && s[i]!=s[border])
+        border=arr[border-1];
+     
+     if(s[i]==s[border])
+         border++;
+      else
+       border=0;
+      
+      arr[i]=border;
+    }
+}//send mod-2 for a^-1 if mod is a prime number
+
+ll mod=1000000007;
+ll add( ll a , ll b)
+{
+    return (((a%mod)+(b%mod))%mod);
+}
+ll mul(ll a,ll b)
+{
+  return (((a%mod)*(b%mod))%mod);
+}
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mul(res, a);
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+ll subs(ll a,ll b)
+{
+    return (((a%mod)-(b%mod)+mod)%mod);
+}
+ll dv(ll a,ll b)
+{
+    ll inv=binpow(b,mod-2);
+    return mul(a,inv);
+}
+
+ll dsu_arr[100000];
+ll dsu_sz[100000];
+void dsu(ll n)
+{
+    for(ll i=0;i<=n;i++)
+    {
+        dsu_arr[i]=i;
+        dsu_sz[i]=1;
+    }
+}
+ll find(ll x)
+{
+     ll root=x;
+    while (root!=dsu_arr[root])
+    {
+        root=dsu_arr[root];
+    }
+    while(x!=dsu_arr[x])
+    {
+        dsu_arr[x]=root;
+        x=dsu_arr[x];
+    }
+    return root;
+}
+ll merge(ll x,ll y)
+{
+    ll root1=find(x);
+    ll root2=find(y);
+    
+    if(root1==root2)
+    return 0ll;
+ 
+    if(dsu_sz[x]>dsu_sz[y]){
+    dsu_arr[root2]=root1;
+    dsu_sz[root1]+=dsu_sz[root2];
+    }
+    else
+    {
+         dsu_sz[root2]+=dsu_sz[root1];
+        dsu_arr[root1]=root2;
+    }
+    return 1ll;
+}
+/*
+vector<ll>adj[100005];
+bool vis[100005];
+ll dist[100005];
+void bfs(ll c)
+{
+  vis[c]=true;
+  dist[c]=0;
+  q.push(c);
+while(!q.empty())
+{
+    ll x=q.front();
+    q.pop();
+   
+    for(ll i=0;i<adj[x].size();i++)
+    {
+        ll y=adj[x][i];
+        if(!vis[y])
+        {
+            vis[y]=true;
+            dist[y]=dist[x]+1;
+            
+            q.push(y);
+        }
+    }
+ 
+}
+}
+*/
+
+int main()
+{
+    Fast
+ll n;
+cin>>n;
+ll dp[n+5];
+
+for(ll i=0;i<=n;i++)
+{
+    dp[i]=0;
+}
+for(ll i=3;i<=n;i++)
+{
+   
+    for(ll j=3;j<=i;j++)
+    {
+        if(j==i)
+        {
+            if(j>=3)
+            dp[i]=add(dp[i],1ll);
+        }
+        else
+      dp[i]=add(dp[i],dp[i-j]);
+      
+    }
+   
+}
+cout<<dp[n]<<endl;
+
+
+}
+
+
+
+   
+      
+    
+        
+
