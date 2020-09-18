dda42185c687bc8f9e29d785c076819d00eb9ffa
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-09-18 09:24:00 +0600

    Add solution for problem `C - Ladder` Link: https://codeforces.com/contest/279/problem/C Tags: dp, implementation, two pointers, *1700 Ref: https://codeforces.com/contest/279/submission/93086180

---
+++ codeforces/279/C.cpp
@@ -0,0 +1,223 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define f first
+#define s second
+#define Fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+typedef pair<ll , pair<ll, ll> > pi;
+int pow(int x,int y){
+    int res=1;
+    while(y){
+        if(y&1) res*=x;
+        y>>=1;
+        x*=x;
+    }
+    return res;
+}   
+struct Compare {
+    constexpr bool operator()(pi const & a,
+                              pi const & b) const noexcept
+    { return a.first < b.first || (a.first == b.first && a.second.first > b.second.first); }
+ 
+};
+void prefix_function( string s,ll arr[] )
+{
+     long long border=0; arr[0]=0;
+    for(long long i=1;i<s.size();i++)
+    {
+      while(border>0 && s[i]!=s[border])
+        border=arr[border-1];
+     
+     if(s[i]==s[border])
+         border++;
+      else
+       border=0;
+      
+      arr[i]=border;
+    }
+}//send mod-2 for a^-1 if mod is a prime number
+
+ll mod=998244353;
+ll add( ll a , ll b)
+{
+    return (((a%mod)+(b%mod))%mod);
+}
+ll mul(ll a,ll b)
+{
+  return (((a%mod)*(b%mod))%mod);
+}
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mul(res, a);
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+ll subs(ll a,ll b)
+{
+    return (((a%mod)-(b%mod)+mod)%mod);
+}
+ll dv(ll a,ll b)
+{
+    ll inv=binpow(b,mod-2);
+    return mul(a,inv);
+}
+
+ll dsu_arr[100000];
+ll dsu_sz[100000];
+void dsu(ll n)
+{
+    for(ll i=0;i<=n;i++)
+    {
+        dsu_arr[i]=i;
+        dsu_sz[i]=1;
+    }
+}
+ll find(ll x)
+{
+     ll root=x;
+    while (root!=dsu_arr[root])
+    {
+        root=dsu_arr[root];
+    }
+    while(x!=dsu_arr[x])
+    {
+        dsu_arr[x]=root;
+        x=dsu_arr[x];
+    }
+    return root;
+}
+ll merge(ll x,ll y)
+{
+    ll root1=find(x);
+    ll root2=find(y);
+    
+    if(root1==root2)
+    return 0ll;
+ 
+    if(dsu_sz[x]>dsu_sz[y]){
+    dsu_arr[root2]=root1;
+    dsu_sz[root1]+=dsu_sz[root2];
+    }
+    else
+    {
+         dsu_sz[root2]+=dsu_sz[root1];
+        dsu_arr[root1]=root2;
+    }
+    return 1ll;
+}
+/*
+vector<ll>adj[100005];
+bool vis[100005];
+ll dist[100005];
+void bfs(ll c)
+{
+  vis[c]=true;
+  dist[c]=0;
+  q.push(c);
+while(!q.empty())
+{
+    ll x=q.front();
+    q.pop();
+   
+    for(ll i=0;i<adj[x].size();i++)
+    {
+        ll y=adj[x][i];
+        if(!vis[y])
+        {
+            vis[y]=true;
+            dist[y]=dist[x]+1;
+            
+            q.push(y);
+        }
+    }
+ 
+}
+}
+*/
+
+int main()
+{
+    Fast
+ ll x,y;
+ cin>>x>>y;
+ vector<ll>sura;
+ for(ll i=0;i<x;i++)
+ {
+     ll k;
+     cin>>k;
+     sura.push_back(k);
+ }
+ vector<ll>dp(x+1);
+ dp[0]=1;
+ bool decrease=false;
+ bool increase=false;
+
+ ll e=1;
+ for(ll i=1;i<x;i++)
+ {
+     if(sura[i-1]>sura[i])
+     {
+        decrease=true;
+        increase=false;
+     
+        dp[i]=dp[i-1]+1;
+        e=1;
+     }
+     else if(sura[i-1]<sura[i])
+     {
+         if(decrease)
+         {
+             dp[i]=e+1;
+
+         }
+         else if(!decrease)
+         {
+              dp[i]=dp[i-1]+1;
+         }
+         
+         increase=true;
+         decrease=false;
+         e=1;
+     }
+     else
+     {
+         dp[i]=dp[i-1]+1;
+         e++;
+     }
+     
+     
+ }
+while(y--)
+{
+    ll l,r;
+    cin>>l>>r;
+    ll sz=r-l+1;
+    r--;
+    if(dp[r]>=sz)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    
+}
+
+
+}
+
+
+
+   
+      
+    
+        
+
