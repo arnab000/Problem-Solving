d7a66726247182fa46d43528b3ba6d224b24b3aa
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-09-25 05:01:00 +0600

    Add solution for problem `C2 - Pokémon Army (hard version)` Link: https://codeforces.com/contest/1420/problem/C2 Tags: data structures, divide and conquer, dp, greedy, implementation, *2100 Ref: https://codeforces.com/contest/1420/submission/93737460

--- codeforces/1420/C2.cpp
+++ codeforces/1420/C2.cpp
@@ -1,13 +1,6 @@
-
-
-
-   #pragma GCC optimize("Ofast")
-   #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
- #pragma GCC optimize("unroll-loops")
-
-
-
-
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
 
 #include<bits/stdc++.h>
 using namespace std;
