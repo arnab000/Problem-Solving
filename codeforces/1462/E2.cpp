423d166f9027a8565b8a3021224a990fd065c49c
Arnab Saha <arnab.saintjoseph@gmail.com>
2020-12-16 00:34:00 +0600

    Add solution for problem `E2 - Close Tuples (hard version)` Link: https://codeforces.com/contest/1462/problem/E2 Tags: binary search, combinatorics, implementation, math, sortings, two pointers, *1700 Ref: https://codeforces.com/contest/1462/submission/101359440

---
+++ codeforces/1462/E2.cpp
@@ -0,0 +1,301 @@
+/*#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define f first
+#define s second
+#define Fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+typedef pair<ll , pair<ll, ll> > pi;
+int pow(int x,int y){
+    int res=1;
+    while(y){
+        if(y&1) res*=x;
+        y>>=1;
+        x*=x;
+    }
+    return res;
+}   
+struct Compare {
+    constexpr bool operator()(pi const & a,
+                              pi const & b) const noexcept
+    { return a.first < b.first || (a.first == b.first && a.second.first > b.second.first); }
+ 
+};
+void prefix_function( string s,ll arr[] )
+{
+     long long border=0; arr[0]=0;
+    for(long long i=1;i<s.size();i++)
+    {
+      while(border>0 && s[i]!=s[border])
+        border=arr[border-1];
+     
+     if(s[i]==s[border])
+         border++;
+      else
+       border=0;
+      
+      arr[i]=border;
+    }
+}//send mod-2 for a^-1 if mod is a prime number
+
+ll mod=1e9 + 7;
+ll add( ll a , ll b)
+{
+    return (((a%mod)+(b%mod))%mod);
+}
+ll mul(ll a,ll b)
+{
+  return (((a%mod)*(b%mod))%mod);
+}
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mul(res, a);
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+ll subs(ll a,ll b)
+{
+    return (((a%mod)-(b%mod)+mod)%mod);
+}
+ll dv(ll a,ll b)
+{
+    ll inv=binpow(b,mod-2);
+    return mul(a,inv);
+}
+
+ll dsu_arr[100000];
+ll dsu_sz[100000];
+void dsu(ll n)
+{
+    for(ll i=0;i<=n;i++)
+    {
+        dsu_arr[i]=i;
+        dsu_sz[i]=1;
+    }
+}
+ll find(ll x)
+{
+     ll root=x;
+    while (root!=dsu_arr[root])
+    {
+        root=dsu_arr[root];
+    }
+    while(x!=dsu_arr[x])
+    {
+        dsu_arr[x]=root;
+        x=dsu_arr[x];
+    }
+    return root;
+}
+ll merge(ll x,ll y)
+{
+    ll root1=find(x);
+    ll root2=find(y);
+    
+    if(root1==root2)
+    return 0ll;
+ 
+    if(dsu_sz[x]>dsu_sz[y]){
+    dsu_arr[root2]=root1;
+    dsu_sz[root1]+=dsu_sz[root2];
+    }
+    else
+    {
+         dsu_sz[root2]+=dsu_sz[root1];
+        dsu_arr[root1]=root2;
+    }
+    return 1ll;
+}
+/*
+vector<ll>adj[100005];
+bool vis[100005];
+ll dist[100005];
+void bfs(ll c)
+{
+  vis[c]=true;
+  dist[c]=0;
+  queue<ll>q;
+  q.push(c);
+while(!q.empty())
+{
+    ll x=q.front();
+    q.pop();
+   
+    for(ll i=0;i<adj[x].size();i++)
+    {
+        ll y=adj[x][i];
+        if(!vis[y])
+        {
+            vis[y]=true;
+            dist[y]=dist[x]+1;
+            
+            q.push(y);
+        }
+    }
+ 
+}
+}
+*/
+#define mx 2000005
+ll fin[mx];
+    ll n;
+
+void ad(ll p,ll e )
+{
+
+   for(ll i=p;i<=n+2;i+=(i&-i))
+   {
+     if(e==1)
+     fin[i]=add(fin[i],1ll);
+     else
+     fin[i]=subs(fin[i],1ll);
+    
+   }
+
+}
+ll sum(ll p)
+{
+    if(p<=0)
+    return 0ll;
+    ll ans=0;
+   for(int i=p;i;i-=(i&-i))
+   {
+   ans=add(ans,fin[i]);
+    }
+return ans%mod;
+}
+ long long fac[200006];
+ long long power( long long x, 
+                                  ll y, ll p)
+{
+    long long res = 1; // Initialize result
+ 
+    x = x % p; // Update x if it is more than or
+    // equal to p
+ 
+    while (y > 0) 
+    {
+     
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res * x) % p;
+ 
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
+ 
+// Returns n^(-1) mod p
+ long long modInverse( long long n,  ll p)
+{
+    return power(n, p - 2, p);
+}
+ 
+// Returns nCr % p using Fermat's little
+// theorem.
+ long long nCrModPFermat( long long n,
+                                 ll r, ll p)
+{
+    // If n<r, then nCr should return 0
+    if (n < r)
+        return 0;
+    // Base case
+    if (r == 0)
+        return 1;
+ 
+    // Fill factorial array so that we
+    // can find all factorial of r, n
+    // and n-r
+  
+    return (fac[n] * modInverse(fac[r], p) % p
+            * modInverse(fac[n - r], p) % p)
+           % p;
+}
+
+
+int main()
+{
+    Fast
+ ll test;
+ cin>>test;
+     fac[0] = 1;
+    ll md = 1e9 + 7;
+    for (ll i = 1; i <= 200005; i++)
+    fac[i] = (fac[i - 1] * i) % md;
+ while(test--)
+ {
+    
+    ll m, l;
+     cin>>n>>m>>l;
+  
+     vector<ll>sura;
+     ll ans=0;
+     for(ll i=0;i<n;i++)
+     {
+         ll k;
+         cin>>k;
+        sura.push_back(k);
+
+        
+     }
+    
+     sort(sura.begin(),sura.end());
+             for (int i = 0; i < n; i++)
+        {
+            int curr = sura[i];
+            int upper = curr + l;
+            int right = upper_bound(sura.begin(),sura.end(), upper) - sura.begin() - 1;
+            ans = (ans % mod + nCrModPFermat(right - i, m - 1, mod) % mod) % mod;
+        }
+       
+     
+     cout<<ans<<'\n';
+
+ }
+
+
+}
+
+
+
+   
+      
+    
+        
+
